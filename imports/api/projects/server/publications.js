faf831c2e2466e5b6a46211ab2d3cdc4bb939594
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2018-07-25 19:31:18 +0200

    on recupere les users concernés

--- imports/api/projects/server/publications.js
+++ imports/api/projects/server/publications.js
@@ -26,9 +26,36 @@ publishComposite("project", function(projectId) {
             // tasks
             find(list) {
               return Tasks.find({ listId: list._id}, {sort: {order: 1}});
+            },
+            children: [
+              {
+                find (task) {
+                  var userIds = [
+                    task.createdBy
+                  ]
+                  if (task.notes) {
+                    task.notes.map( note => {
+                      userIds.push(note.createdBy)
+                    });
+                  }
+
+                  return Meteor.users.find({ _id: {$in: userIds }},{ fields: { profile: 1 } });  
+                }
+              }
+            ]
+            
+          }, {
+            find (list) {
+              return Meteor.users.find({ _id: list.createdBy },{ fields: { profile: 1 } });
             }
           }
+
         ]
+      },
+      {
+        find (project) {
+          return Meteor.users.find({ _id: project.createdBy },{ fields: { profile: 1 } });
+        }
       }
     ]
   }
