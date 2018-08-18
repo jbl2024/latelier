284f54605600b7b8089ee98783e2da3e2e4ad7bb
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2018-08-18 10:55:34 +0200

    ux du register

--- imports/ui/pages/Auth/Register.vue
+++ imports/ui/pages/Auth/Register.vue
@@ -1,174 +1,154 @@
 <template>
   <div class="register">
-    <div class="auth-layout_inner">
-      <el-card class="auth-layout_card card">
-        <header class="auth-layout_card-header card-header">
-          <h2>Create Account</h2>
-        </header>
-        <span class="divider"></span>
-        <el-form
-          :model="registerForm"
-          :rules="rules"
-          ref="registerForm"
-          class="auth-layout_card-content card-content">
-          <el-row :gutter="20" class="form-row-item">
-            <el-col :span="12">
-              <el-form-item
-                prop="firstName"
-                label="First Name">
-                <el-input
-                  v-model="registerForm.firstName"
-                  auto-complete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item
-                prop="lastName"
-                label="Last Name">
-                <el-input
-                  v-model="registerForm.lastName"
-                  auto-complete="off"></el-input>
-              </el-form-item>
-            </el-col>
-          </el-row>
-          <el-row :gutter="20" class="form-row-item">
-            <el-col :span="12">
-              <el-form-item
-                prop="email"
-                label="Email">
-                <el-input
-                  v-model="registerForm.email"
-                  auto-complete="off"></el-input>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12">
-              <el-form-item
-                prop="phone"
-                label="Callphone">
-                <el-input
-                  v-model="registerForm.phone"
-                  auto-complete="off"></el-input>
-              </el-form-item>
-            </el-col>
-          </el-row>
-          <el-row :gutter="0" class="form-row-item">
-            <el-form-item
-              label="Password"
-              prop="password">
-              <el-input
-                type="password"
-                v-model="registerForm.password"
-                auto-complete="off"></el-input>
-            </el-form-item>
-          </el-row>
-          <el-row :gutter="0" class="form-row-item">
-            <el-form-item>
-              <el-button
-                class="auth-layout_submit full-width"
-                size="large"
-                type="primary"
-                :loading="isLoading"
-                @click="submitForm('registerForm')">
-                Register
-              </el-button>
-            </el-form-item>
-          </el-row>
-        </el-form>
-      </el-card>
-      <footer class="auth-layout_footer">
-        Already have a register?
-        <router-link :to="{ name: 'login'}">Login</router-link>
-      </footer>
-    </div>
+    <div class="centered-container">
+    <form novalidate class="md-layout" @submit.prevent="validateRegister">
+      <md-card>
+        <md-card-header>
+          <div class="md-title">Créer un compte</div>
+        </md-card-header>
 
+        <md-card-content>
+          <label for="email" class="label">Email</label>
+          <md-field :class="getValidationClass('email')">
+            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+          </md-field>
+
+          <label for="password" class="label">Mot de passe</label>
+          <md-field :class="getValidationClass('password')">
+            <md-input type="password" name="password" id="password" autocomplete="password" v-model="form.password" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.password.required">The password is required</span>
+            <span class="md-error" v-else-if="!$v.form.password.minLenght">Invalid email</span>
+          </md-field>
+
+        </md-card-content>
+
+        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary" :disabled="sending">Créer un compte</md-button>
+        </md-card-actions>
+        <md-card-actions>
+          <md-button :to="{ name: 'login'}">Déjà un compte ?</md-button>
+          <md-button :to="{ name: 'forgot-password'}">Mot de passe oublié ?</md-button>
+        </md-card-actions>
+      </md-card>
+
+      <md-snackbar :md-active.sync="notify">{{ notifyText }}</md-snackbar>
+    </form>
+    </div>
   </div>
 </template>
-
 <script>
+  import { validationMixin } from 'vuelidate'
+  import {
+    required,
+    email,
+    minLength,
+    maxLength
+  } from 'vuelidate/lib/validators'
+
   export default {
-    name: 'auth-register',
-    data: () => ({
-      isLoading: false,
-      registerForm: {
-        username: '',
-        password: '',
-        email: '',
-        firstName: '',
-        lastName: '',
-        phone: '',
+    name: 'login-widget',
+    mixins: [validationMixin],
+      data: () => ({
+      form: {
+        email: null,
+        password: null,
       },
-      rules: {
-        email: [
-          { required: true, message: 'Please insert your email', trigger: 'blur' },
-          { type: 'email', message: 'Please insert is valid email!', trigger: 'blur,change' }
-        ],
-        password: [
-          { required: true, message: 'Please insert your password!', trigger: 'blur' }
-        ],
-        firstName: [
-          { required: true, message: 'Please insert your First name!', trigger: 'blur' }
-        ],
-        lastName: [
-          { required: true, message: 'Please insert your Last name!', trigger: 'blur' }
-        ],
-        phone: [
-          { required: true, message: 'Please insert your callphone!', trigger: 'blur' }
-        ]
-      }
+      notify: false,
+      notifyText: '',
+      sending: false,
     }),
+    mounted () {
+      this.$store.dispatch('setHideDrawer', true);    
+    },
+    beforeDestroy () {
+      this.$store.dispatch('setHideDrawer', false);    
+    },
+    validations: {
+      form: {
+        password: {
+          required,
+          minLength: minLength(3)
+        },
+        email: {
+          required,
+          email
+        }
+      }
+    },
     methods: {
-      submitForm(formName) {
-        this.isLoading = true
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            let dataForm = this.registerForm
-            let userData = {
-              createdAt: new Date(),
-              password: dataForm.password,
-        			email: dataForm.email,
-        			profile: {
-        				firstName: dataForm.firstName,
-        				lastName: dataForm.lastName,
-        				phone: dataForm.phone
-        			}
-        		}
-            Accounts.createUser(userData, (err) => {
-        			if(err){
-                this.$notify.error({
-                  title: 'Sorry',
-                  message: err.reason,
-                  offset: 100
-                })
-                this.isLoading = false
-        			} else {
-                this.$notify.success({
-                  title: 'Success',
-                  message: 'User create success!',
-                  offset: 100
-                })
-                this.isLoading = true
-                this.$router.push({ name: 'login' })
-        			}
-        		})
-          } else {
-            this.$notify.error({
-              title: 'Sorry!!!',
-              message: 'All fields are required',
-              offset: 100
-            })
-            this.isLoading = false
+      getValidationClass (fieldName) {
+        const field = this.$v.form[fieldName]
+
+        if (field) {
+          return {
+            'md-invalid': field.$invalid && field.$dirty
           }
-        })
+        }
       },
-      resetForm(formName) {
-        this.$refs[formName].resetFields()
+      clearForm () {
+        this.$v.$reset()
+        this.form.email = null;
+        this.form.password = null;
+        this.notify = false;
+      },
+      register () {
+        this.sending = true;
+
+        const userData = {
+          createdAt: new Date(),
+          password: this.form.password,
+          email: this.form.email
+        }
+        Accounts.createUser(userData, (err) => {
+          this.sending = false;
+          this.notify = false;
+          if(err){
+            this.notifyText = 'Erreur ' + err.reason;
+            this.notify = true;
+          } else {
+            Meteor.loginWithPassword(this.form.email, this.form.password, (err) => {
+              if (err) {
+                this.notifyText = 'Erreur ' + err.reason;
+                this.notify = true;
+              } else {
+                this.clearForm();
+                this.$router.push({ name: 'projects-page' })
+              }
+            });
+          }          
+        });
+      },
+      validateRegister () {
+        this.$v.$touch()
+        if (!this.$v.$invalid) {
+          this.register()
+        }
       }
     }
   }
 </script>
 
 <style scoped>
-.register {
-  width: 600px;
+
+.centered-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: calc(100vh - 64px);
+}
+
+.md-card {
+  text-align: left;
+  margin: 0 auto;
+}
+
+.md-title {
+  text-align: center;
 }
 </style>
+
