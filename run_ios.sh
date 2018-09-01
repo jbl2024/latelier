09e0ee950abdfc4d78e88fff69d5063bc71d92f3
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2018-09-01 09:23:16 +0200

    ajout variables hmr pour ios

--- run_ios.sh
+++ run_ios.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+ip=`ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}'`
+export HMR_URL=$ip
+export HMR_PORT=3003
 echo "Starting app on ios device..."
+echo "HMR_URL=$HMR_URL | HMR_PORT=$HMR_PORT"
 meteor run ios-device --settings settings-development.json
