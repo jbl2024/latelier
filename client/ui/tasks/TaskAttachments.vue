b8982151b6d5006f7f0a88046feefefc5327da2d
Jérôme Blondon <jerome@blondon.fr>
2018-09-16 19:59:22 +0200

    Affichage des pj

--- client/ui/tasks/TaskAttachments.vue
+++ client/ui/tasks/TaskAttachments.vue
@@ -1,30 +1,44 @@
 <template>
 
 <div class="task-attachments">
-    <md-field v-show="!isUploading">
+    <md-field >
       <label>Single</label>
-      <md-file v-model="file" @md-change="onUpload"/>
+      <md-file v-model="file" @md-change="onUpload" :disabled="isUploading"/>
     </md-field>
-    <md-progress-spinner md-mode="indeterminate" v-show="isUploading"></md-progress-spinner>
+
+   <md-list>
+      <md-list-item v-for="attachment in attachments" :key="attachment._id">
+        <md-icon>description</md-icon>
+        <span class="md-list-item-text">{{ attachment.name }}</span>
+
+        <md-button class="md-icon-button md-list-action">
+          <md-icon>delete</md-icon>
+        </md-button>
+      </md-list-item>
+   </md-list>
+   <div class="progress">
+    <md-progress-spinner :md-diameter="30" :md-stroke="3" md-mode="indeterminate" v-show="isUploading"></md-progress-spinner>
+   </div>
+
 
 </div>
 
 </template>
 
 <script>
-import { Projects } from '/imports/api/projects/projects.js'
-import { Lists } from '/imports/api/lists/lists.js'
-import { Tasks } from '/imports/api/tasks/tasks.js'
+import { Projects } from "/imports/api/projects/projects.js";
+import { Lists } from "/imports/api/lists/lists.js";
+import { Tasks } from "/imports/api/tasks/tasks.js";
 import { Attachments } from "/imports/api/attachments/attachments";
 
-import moment from 'moment';
-import 'moment/locale/fr'
+import moment from "moment";
+import "moment/locale/fr";
 
 export default {
-  name: 'task-attachments',
+  name: "task-attachments",
   props: {
     task: {
-      type: Object,
+      type: Object
     }
   },
   data() {
@@ -34,33 +48,48 @@ export default {
     };
   },
   methods: {
-    onUpload (files) {
+    onUpload(files) {
       var that = this;
-      const upload = Attachments.insert({
-        file: files[0],
-        streams: 'dynamic',
-        chunkSize: 'dynamic',
-        meta: {
-          projectId: this.task.projectId,
-          taskId: this.task._id,
-          createdBy: Meteor.userId()
-        }
-      }, false);
+      const upload = Attachments.insert(
+        {
+          file: files[0],
+          streams: "dynamic",
+          chunkSize: "dynamic",
+          meta: {
+            projectId: this.task.projectId,
+            taskId: this.task._id,
+            createdBy: Meteor.userId()
+          }
+        },
+        false
+      );
 
-      upload.on('start', function () {
+      upload.on("start", function() {
         that.isUploading = true;
       });
 
-      upload.on('end', function (error, fileObj) {
+      upload.on("end", function(error, fileObj) {
         that.isUploading = false;
         if (error) {
-          alert('Error during upload: ' + error);
+          alert("Error during upload: " + error);
         } else {
-          alert('File "' + fileObj.name + '" successfully uploaded');
+          that.file = null;
         }
       });
 
-      upload.start();      
+      upload.start();
+    }
+  },
+  meteor: {
+    attachments: {
+      params () {
+        return {
+          task: this.task
+        };
+      },
+      update ({task}) {
+        return Attachments.find({'meta.taskId': this.task._id});
+      }
     }
   }
 };
@@ -68,4 +97,7 @@ export default {
 
 <style scoped>
 
+.progress {
+  text-align: center;
+}
 </style>
