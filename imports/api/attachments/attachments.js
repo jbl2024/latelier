07031ed713cb26d73ce1eb1384d888141d873aa2
Jérôme Blondon <jerome@blondon.fr>
2018-09-21 22:19:07 +0200

    on peut spécifier un chemin pour les pieces jointes

--- imports/api/attachments/attachments.js
+++ imports/api/attachments/attachments.js
@@ -4,6 +4,7 @@ import { check } from "meteor/check";
 
 export const Attachments = new FilesCollection({
   collectionName: "Attachments",
+  storagePath: Meteor.settings.attachmentsPath || 'assets/app/uploads',
   allowClientCode: true, // Disallow remove files from Client
   onBeforeUpload(file) {
     return true;
@@ -12,7 +13,7 @@ export const Attachments = new FilesCollection({
     if (Meteor.isServer) {
       this.update({ _id: file._id }, { $set: {'meta.createdAt': new Date()}});
     }
-  }
+  } 
 });
 
 
