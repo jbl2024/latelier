a8b86ebb1a3bccd9791751d5bd4b963d5df022e1
Jérôme Blondon <jerome@blondon.fr>
2018-09-22 10:13:49 +0200

    wip sur les labels

---
+++ imports/api/labels/labels.js
@@ -0,0 +1,55 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+
+export const Labels = new Mongo.Collection('labels');
+
+Meteor.methods({
+  'labels.create'(projectId, name, color) {
+    check(projectId, String);
+    check(name, String);
+    check(color, String);
+
+    // Make sure the user is logged in before inserting a task
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    var labelId = Labels.insert({
+      projectId: projectId,
+      name: name,
+      color: color,
+      createdAt: new Date(),
+      createdBy: Meteor.userId()
+    });
+
+    return labelId;
+  },
+
+  'labels.remove'(labelId) {
+    check(labelId, String);
+
+    Labels.remove(labelId);
+  },
+
+  'labels.updateName'(labelId, name) {
+    check(labedId, String);
+    check(name, String);
+    if (name.length == 0) {
+      throw new Meteor.Error('invalid-name');
+    }
+
+    Labels.update({_id: labelId}, {$set: {name: name}});
+  },
+
+  'labels.updateColor'(labelId, color) {
+    check(labedId, String);
+    check(color, String);
+    if (color.length == 0) {
+      throw new Meteor.Error('invalid-color');
+    }
+
+    Labels.update({_id: labelId}, {$set: {color: color}});
+  },
+
+});
