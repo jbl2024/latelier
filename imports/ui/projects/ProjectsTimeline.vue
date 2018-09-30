302a9cd42080acdebf017ae57daa6a54dcb91e33
Jérôme Blondon <jerome@blondon.fr>
2018-09-30 17:59:26 +0200

    wip

--- imports/ui/projects/ProjectsTimeline.vue
+++ imports/ui/projects/ProjectsTimeline.vue
@@ -70,12 +70,14 @@ export default {
     this.debouncedFilter = debounce((val) => { this.filter = val}, 400);
   },
   mounted () {
+    this.$store.dispatch('setCurrentOrganizationId', this.organizationId);    
     this.$store.dispatch('setShowCategories', true);    
     this.$events.listen('close-project-detail', task => {
       this.showProjectDetail = false;
     });
   },
   beforeDestroy() {
+    this.$store.dispatch('setCurrentOrganizationId', 0);    
     this.$store.dispatch('setShowCategories', false);    
     this.$events.off('close-project-detail');
   },
