d5c22db6ac72df2d704e1b123b8de2fd9a854c0d
Jérôme Blondon <jerome@blondon.fr>
2018-10-19 22:20:16 +0200

    vuetification du selecteur de date

--- client/ui/widgets/SelectDate.vue
+++ client/ui/widgets/SelectDate.vue
@@ -1,57 +1,58 @@
 <template>
   <div class="select-date">
-
-    <md-dialog :md-active.sync="active">
-      <md-dialog-title>Choisir une date</md-dialog-title>
-
-      <div class="content">
-        <flat-pickr v-model="fakeDate" :config="config" class="invisible" @on-change="onChangeEvent"></flat-pickr>
-      </div>
-      <md-dialog-actions>
-        <md-button class="md-button" @click="closeDialog">Annuler</md-button>
-        <md-button class="md-button md-primary" @click="selectDate">Confirmer</md-button>
-      </md-dialog-actions>
-    </md-dialog>  
-  </div>    
+    <v-dialog v-model="active" max-width="385" persistent :fullscreen="$vuetify.breakpoint.xsOnly">
+      <v-card>
+        <v-card-title class="headline">Choisir une date</v-card-title>
+        <v-card-text>
+        <div><flat-pickr v-model="fakeDate" :config="config" class="invisible" @on-change="onChangeEvent"></flat-pickr></div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat @click="closeDialog">Annuler</v-btn>
+          <v-btn color="info" @click="selectDate">Sélectionner</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
-import { Meteor } from 'meteor/meteor'
-import { Projects } from '/imports/api/projects/projects.js'
-import flatPickr from 'vue-flatpickr-component';
-import confirmDatePlugin from 'flatpickr/dist/plugins/confirmDate/confirmDate.js';
-import 'flatpickr/dist/flatpickr.css';
-import {French} from 'flatpickr/dist/l10n/fr';
+import { Meteor } from "meteor/meteor";
+import { Projects } from "/imports/api/projects/projects.js";
+import flatPickr from "vue-flatpickr-component";
+import confirmDatePlugin from "flatpickr/dist/plugins/confirmDate/confirmDate.js";
+import "flatpickr/dist/flatpickr.css";
+import { French } from "flatpickr/dist/l10n/fr";
 
 export default {
   props: {
     active: Boolean,
     disableTime: Boolean
   },
-  data () {
+  data() {
     return {
       fakeDate: null,
       date: null,
       config: {
         enableTime: !this.disableTime,
         altInput: true,
-        altFormat: 'd/m/Y H:i',
+        altFormat: "d/m/Y H:i",
         weekNumbers: true,
         inline: true,
         time_24hr: true,
-        locale: French,
-      },             
-    }
+        locale: French
+      }
+    };
   },
   components: {
     flatPickr
   },
   methods: {
-    closeDialog () {
-      this.$emit('update:active', false);
+    closeDialog() {
+      this.$emit("update:active", false);
     },
 
-    onChangeEvent (dates) {
+    onChangeEvent(dates) {
       if (dates && dates.length > 0) {
         this.date = dates[0];
       } else {
@@ -59,12 +60,12 @@ export default {
       }
     },
 
-    selectDate () {
-      this.$emit('update:active', false);
-      this.$emit('select', this.date);
+    selectDate() {
+      this.$emit("update:active", false);
+      this.$emit("select", this.date);
     }
   }
-}
+};
 </script>
 
 <style>
