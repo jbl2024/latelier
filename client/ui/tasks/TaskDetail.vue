d9a3a78b37e1385656ef5071ac0fa4cebdaf61d9
Jérôme Blondon <jerome@blondon.fr>
2018-10-19 21:04:42 +0200

    wip

--- client/ui/tasks/TaskDetail.vue
+++ client/ui/tasks/TaskDetail.vue
@@ -1,84 +1,93 @@
 <template>
 
-<div class="task-detail">
-  <md-toolbar class="md-transparent" md-elevation="0">
-    <div class="md-toolbar-row">
-      <div class="md-toolbar-section-start">
-        <md-button class="md-icon-button md-dense" @click="requestClose()" v-shortkey="['esc']" @shortkey="requestClose()">
-          <md-icon>close</md-icon>
-        </md-button>
+  <div class="task-detail">
+    <v-toolbar dark color="primary">
+        <v-btn icon flat @click="requestClose()" v-shortkey="['esc']" @shortkey="requestClose()">
+          <v-icon>close</v-icon>
+        </v-btn>
+      <v-toolbar-title>
         <span>{{ task.name}}</span>
-      </div>
-      <div class="md-toolbar-section-end">
-        <md-menu md-size="medium" md-align-trigger class="settings" :mdCloseOnClick="true" :mdCloseOnSelect="true">
-          <md-button md-menu-trigger class="md-icon-button">
-            <md-icon>more_vert</md-icon>
-          </md-button>
-          <md-menu-content>
-            <md-menu-item @click="deleteTask(task._id)">Supprimer</md-menu-item>
-          </md-menu-content>
-        </md-menu>
-      </div>
-    </div>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu bottom left>
+        <v-btn slot="activator" icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-tile @click="deleteTask(task._id)">
+            <v-list-tile-title>Supprimer</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+
     <task-labels :task="task"></task-labels>
     <div class="md-toolbar-row">
       <author-line :user-id="task.createdBy" :date="task.createdAt"></author-line>
     </div>
 
-  </md-toolbar>
+    <v-divider></v-divider>
+    <div class="description">
+      <div v-show="!editDescription && task.description && task.description.length > 0" @click="startEditDescription">
+        <div v-html="markDown(task.description)"></div>
+      </div>
+      <div v-show="!task.description && !editDescription" @click="startEditDescription">
+        Aucune description
+      </div>
 
-  <md-divider></md-divider>
-  <div class="description">
-    <div v-show="!editDescription && task.description && task.description.length > 0" @click="startEditDescription">
-      <div v-html="markDown(task.description)"></div>
-    </div>
-    <div v-show="!task.description && !editDescription" @click="startEditDescription">
-      Aucune description
-    </div>
+      <div v-show="editDescription">
+        <md-field>
+          <label>Description</label>
+          <md-textarea ref="description" v-model="task.description" @keyup.ctrl.enter="updateDescription"></md-textarea>
+        </md-field>
+        <md-button class="md-icon-button" @click.native="updateDescription">
+          <md-icon>check_circle</md-icon>
+        </md-button>
 
-    <div v-show="editDescription">
-      <md-field>
-        <label>Description</label>
-        <md-textarea ref="description" v-model="task.description" @keyup.ctrl.enter="updateDescription"></md-textarea>
-      </md-field>
-      <md-button class="md-icon-button" @click.native="updateDescription">
-        <md-icon>check_circle</md-icon>
-      </md-button>
-
-      <md-button class="md-icon-button" @click.native="cancelUpdateDescription">
-        <md-icon>cancel</md-icon>
-      </md-button>
+        <md-button class="md-icon-button" @click.native="cancelUpdateDescription">
+          <md-icon>cancel</md-icon>
+        </md-button>
+      </div>
     </div>
+    <v-divider></v-divider>
+
+    <v-tabs grow>
+      <v-tab id="tab-properties">
+        Propriétés
+      </v-tab>
+      <v-tab id="tab-notes">
+        {{ getLabel('Notes', notesCount) }}
+      </v-tab>
+      <v-tab id="tab-checklist">
+        {{ getLabel('Checklist', checklistCount) }}
+      </v-tab>
+      <v-tab id="tab-resources">
+        {{ getLabel('Ressources', resourcesCount) }}
+      </v-tab>
+
+      <v-tab-item>
+        <task-properties :task="task"></task-properties>
+      </v-tab-item>
+      <v-tab-item>
+        <task-notes :task="task"></task-notes>
+      </v-tab-item>
+      <v-tab-item>
+        <task-checklist-in-detail :task="task"></task-checklist-in-detail>
+      </v-tab-item>
+      <v-tab-item>
+        <task-resources :task="task"></task-resources>
+      </v-tab-item>
+    </v-tabs> 
+
   </div>
-  <md-divider></md-divider>
-
-  <md-tabs md-sync-route md-alignment="fixed">
-    <md-tab id="tab-properties" md-label="Propriétés">
-      <task-properties :task="task"></task-properties>
-    </md-tab>
-
-    <md-tab id="tab-notes" :md-label="getLabel('Notes', notesCount)">
-      <task-notes :task="task"></task-notes>
-    </md-tab>
-
-    <md-tab id="tab-checklist" :md-label="getLabel('Checklist', checklistCount)">
-      <task-checklist-in-detail :task="task"></task-checklist-in-detail>
-    </md-tab>
-
-    <md-tab id="tab-ressource" :md-label="getLabel('Ressources', resourcesCount)">
-      <task-resources :task="task"></task-resources>
-    </md-tab>
-
-  </md-tabs>  
-</div>
 
 </template>
 
 <script>
-import { Projects } from '/imports/api/projects/projects.js'
-import { Lists } from '/imports/api/lists/lists.js'
-import { Tasks } from '/imports/api/tasks/tasks.js'
-import MarkdownMixin from '/imports/ui/mixins/MarkdownMixin.js'
+import { Projects } from "/imports/api/projects/projects.js";
+import { Lists } from "/imports/api/lists/lists.js";
+import { Tasks } from "/imports/api/tasks/tasks.js";
+import MarkdownMixin from "/imports/ui/mixins/MarkdownMixin.js";
 
 export default {
   mixins: [MarkdownMixin],
@@ -97,76 +106,80 @@ export default {
   },
   meteor: {
     task: {
-      params () {
+      params() {
         return {
           id: this.taskId
         };
       },
       deep: false,
-      update ({id}) {
-        return Tasks.findOne({ _id: id}) || {};
+      update({ id }) {
+        return Tasks.findOne({ _id: id }) || {};
       }
     },
 
     checklistCount: {
-      params () {
+      params() {
         return {
           id: this.taskId
         };
       },
-      update ({id}) {
-        const task = Tasks.findOne({ _id: id}) || {};
+      update({ id }) {
+        const task = Tasks.findOne({ _id: id }) || {};
         const checklist = task.checklist || [];
         return checklist.length;
       }
     },
 
     notesCount: {
-      params () {
+      params() {
         return {
           id: this.taskId
         };
       },
-      update ({id}) {
-        const task = Tasks.findOne({ _id: id}) || {};
+      update({ id }) {
+        const task = Tasks.findOne({ _id: id }) || {};
         const notes = task.notes || [];
         return notes.length;
       }
     },
 
     resourcesCount: {
-      params () {
+      params() {
         return {
           id: this.taskId
         };
       },
-      update ({id}) {
-        const task = Tasks.findOne({ _id: id}) || {};
+      update({ id }) {
+        const task = Tasks.findOne({ _id: id }) || {};
         const resources = task.resources || [];
         return resources.length;
       }
     }
   },
   methods: {
-    requestClose () {
-      this.$events.fire('close-task-detail');
+    requestClose() {
+      this.$events.fire("close-task-detail");
     },
-    deleteTask (taskId) {
-      Meteor.call('tasks.remove', taskId);
-      this.$events.fire('close-task-detail');
+    deleteTask(taskId) {
+      Meteor.call("tasks.remove", taskId);
+      this.$events.fire("close-task-detail");
     },
-    startEditDescription () {
+    startEditDescription() {
       this.savedDescription = this.task.description;
       this.editDescription = true;
       this.$nextTick(() => this.$refs.description.$el.focus());
     },
 
-    updateDescription () {
+    updateDescription() {
       this.editDescription = false;
-      Meteor.call('tasks.updateDescription', this.task._id, this.task.description);
+      Meteor.call(
+        "tasks.updateDescription",
+        this.task._id,
+        this.task.description
+      );
     },
 
-    cancelUpdateDescription () {
+    cancelUpdateDescription() {
       this.editDescription = false;
       this.task.description = this.savedDescription;
     },
@@ -175,41 +188,26 @@ export default {
       if (!count || count == 0) {
         return label;
       }
-      return label + ' (' + count + ')';
+      return label + " (" + count + ")";
     }
   }
 };
 </script>
 
 <style scoped>
-
-.description{
+.description {
   margin: 24px;
 }
 
-.md-tab{
-  margin-left: 0;
-  margin-right: 0;
-  padding-left: 0;
-  padding-right: 0;
-}
-
-.task-notes {
-  margin-left: 0;
-  margin-right: 0;
-}
-
-.task-checklist-in-detail {
+.task-labels {
+  margin-top: 12px;
   margin-left: 12px;
-  margin-right: 12px;
 }
+
 
 pre {
-  font-family: Roboto,Noto Sans,-apple-system,BlinkMacSystemFont,sans-serif;
+  font-family: Roboto, Noto Sans, -apple-system, BlinkMacSystemFont, sans-serif;
   white-space: pre-wrap;
 }
 
-#tab-checklist {
-  min-width: 400px;
-}
 </style>
