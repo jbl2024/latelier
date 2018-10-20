641c8200b05a30ed06a2d377b062bd723ae653c9
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2018-10-20 14:22:19 +0200

    amelioration toolbar

--- client/ui/organizations/OrganizationTitle.vue
+++ client/ui/organizations/OrganizationTitle.vue
@@ -1,16 +1,17 @@
 <template>
 
   <div class="organization-title ml-0 pl-3">
+    <div v-show="!editOrganizationName" @click="startUpdateOrganizationName">
     <slot></slot>
-
-    <v-btn flat icon color="white" :to="{ name: 'organizations-page' }">
+    <v-btn icon flat @click="goTo('organizations-page')">
       <v-icon>home</v-icon>
     </v-btn>
-    <span class="title ml-3 mr-5" v-show="!editOrganizationName" @click="startUpdateOrganizationName">
-        {{ organization.name }}
+    <span class="title ml-3 mr-5">
+      {{ organization.name }}
     </span>
-    <span class="title edit-organization-name" v-show="editOrganizationName">
-      <input @focus="$event.target.select()" type="text" ref="name" v-model="organization.name" v-on:keyup.enter="updateOrganizationName">
+    </div>
+    <span class="title edit" v-show="editOrganizationName">
+      <v-text-field @focus="$event.target.select()" style="width: 500px" flat solo-inverted hide-details prepend-inner-icon="edit" label="Saisir un nom..." ref="name" v-model="organization.name" v-on:keyup.enter="updateOrganizationName"></v-text-field>
       <v-btn icon @click="updateOrganizationName">
         <v-icon>check_circle</v-icon>
       </v-btn>
@@ -34,8 +35,7 @@ export default {
     }
   },
   created() {
-    this.debouncedFilter = debounce(val => {
-    }, 400);
+    this.debouncedFilter = debounce(val => {}, 400);
   },
   meteor: {
     organization: {
@@ -66,29 +66,34 @@ export default {
 
     updateOrganizationName() {
       this.editOrganizationName = false;
-      Meteor.call("organizations.updateName", this.organization._id, this.organization.name);
+      Meteor.call(
+        "organizations.updateName",
+        this.organization._id,
+        this.organization.name
+      );
     },
 
     cancelUpdateOrganizationName() {
       this.editOrganizationName = false;
       this.organization.name = this.savedOrganizationName;
+    },
+
+    goTo(link) {
+      this.$router.push({ name: link });
     }
   }
 };
 </script>
 
 <style scoped>
-.edit-organization-name input {
-  font-size: 20px;
-  font-weight: 400;
-  letter-spacing: 0.02em;
-  margin-top: 6px;
-  padding: 0;
-  font-family: Roboto, Noto Sans, -apple-system, BlinkMacSystemFont, sans-serif;
+
+.edit .v-text-field {
+  float:left;
 }
 
-.edit-organization-name .md-button {
-  margin: 0;
+.title {
+  position: relative;
+  top: 3px;
 }
 
 </style>
