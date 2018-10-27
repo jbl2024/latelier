57c4f310592d21d3f024a0bc5fb8f3b25a9dfbff
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2018-10-27 19:13:02 +0200

    Suppression log

--- imports/startup/server/permissions.js
+++ imports/startup/server/permissions.js
@@ -6,7 +6,6 @@ function initializeRoles() {
   admin.map (email => {
     const user = Accounts.findUserByEmail(email);
     if (user) {
-      console.log (user)
       if (!Roles.userIsInRole(user._id, 'admin', Roles.GLOBAL_GROUP)) {
         console.info('Adding ' + user.emails[0].address + ' to admin role');
         Roles.addUsersToRoles(user._id, 'admin', Roles.GLOBAL_GROUP);
