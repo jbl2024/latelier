e4dea203c39f791c87cc06ccc46c14ad0d79a8f6
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2018-11-06 06:43:22 +0100

    correction asset path

--- imports/api/users/server/emailTemplates.js
+++ imports/api/users/server/emailTemplates.js
@@ -7,7 +7,7 @@ Accounts.emailTemplates.verifyEmail = {
   },
 
   html(user, url) {
-    var email = new MJML(Assets.absoluteFilePath('mjml') + './verifyEmail.mjml');
+    var email = new MJML(Assets.absoluteFilePath('mjml/verifyEmail.mjml'));
     email.helpers({
       url: url,
     });
