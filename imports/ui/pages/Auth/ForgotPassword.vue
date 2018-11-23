428b748bb4541068ab3584a2e11e8e6cf4867a59
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2018-11-23 21:33:00 +0100

    reset & forgot pages

--- imports/ui/pages/Auth/ForgotPassword.vue
+++ imports/ui/pages/Auth/ForgotPassword.vue
@@ -1,94 +1,86 @@
 <template>
   <div class="new-password">
-    <div class="auth-layout_inner">
-      <el-card class="auth-layout_card card">
-        <header class="auth-layout_card-header card-header">
-          <h2>Forgot password</h2>
-        </header>
-        <span class="divider"></span>
-        <el-form
-          :model="forgotPasswordForm"
-          :rules="rules"
-           ref="forgotPasswordForm"
-           class="auth-layout_card-content card-content">
-          <el-form-item
-            prop="email"
-            label="Email">
-            <el-input
-              v-model="forgotPasswordForm.email"
-              auto-complete="off"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button
-              class="auth-layout_submit full-width"
-              size="large"
-              type="primary"
-              @click.prevent="submitForm('forgotPasswordForm')"
-              :loading="isLoading">
-              Send
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </el-card>
-      <footer class="auth-layout_footer">
-        <router-link :to="{ name: 'login'}">Back</router-link>
-      </footer>
+    <div class="centered-container">
+      <v-form v-model="valid" v-on:submit.prevent="validateRecover">
+        <v-card>
+          <v-card-title class="title">{{ $t('Recover password') }}</v-card-title>
+          <v-card-text>
+            <v-text-field
+              label="Email"
+              name="email"
+              id="email"
+              autocomplete="email"
+              :rules="emailRules"
+              v-model="form.email"
+              :disabled="sending"
+            ></v-text-field>
+            <v-progress-linear indeterminate v-if="sending"></v-progress-linear>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" type="submit" :disabled="sending || !valid">{{ $t('Recover') }}</v-btn>
+          </v-card-actions>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn flat :to="{ name: 'login'}">{{ $t('Already have an account?')}}</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn flat :to="{ name: 'register'}">{{ $t('Register') }}</v-btn>
+          </v-card-actions>
+          <v-snackbar v-model="notify">{{ notifyText }}</v-snackbar>
+        </v-card>
+      </v-form>
     </div>
   </div>
 </template>
-
 <script>
-  export default {
-    name: 'auth-forgot-password',
-    data: () => ({
-      isLoading: false,
-      forgotPasswordForm: {
-        email: ''
-      },
-      rules: {
-        email: [
-          { required: true, message: 'Please insert your email!', trigger: 'blur' },
-          { type: 'email', message: 'Please insert is valid email!', trigger: 'blur,change' }
-        ]
-      }
-    }),
-    methods: {
-      submitForm(formName) {
-        this.isLoading = true
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            let dataForm = this.forgotPasswordForm
-            let email = dataForm.email
-            Accounts.forgotPassword({ email }, (err) => {
-              if (err) {
-                this.$notify.error({
-                  title: 'Sorry!',
-                  message: err.reason,
-                  offset: 100
-                })
-                this.isLoading = false
-              } else {
-                this.$notify.success({
-                  title: 'Success',
-                  message: 'A link has been sent to your email!',
-                  offset: 100
-                })
-                this.$router.push({name: 'login'})
-              }
-            })
-          } else {
-            this.$notify.error({
-              title: 'Sorry!',
-              message: 'Email field is required',
-              offset: 100
-            })
-            this.isLoading = false
-          }
-        })
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields()
-      }
+export default {
+  name: "new-password",
+  data: () => ({
+    form: {
+      email: null,
+      password: null
+    },
+    notify: false,
+    notifyText: "",
+    sending: false,
+    valid: false,
+    emailRules: [
+      v => !!v || "L'email est obligatoire",
+      v => v && v.length > 1 || "L'email est invalide"
+    ]
+  }),
+  methods: {
+    clearForm() {
+      this.form.email = null;
+      this.notify = false;
+    },
+    recover() {
+      this.sending = true;
+      const email = this.form.email;
+      Accounts.forgotPassword({ email }, (err) => {
+        this.sending = false;
+        if (err) {
+          this.$store.dispatch("notify", err.reason);
+        } else {
+          this.$store.dispatch("notify", this.$t("A link has been sent to your email!"));
+          this.$router.push({name: 'login'})
+        }
+      })
+    },
+    validateRecover() {
+      this.recover();
     }
   }
+};
 </script>
+
+<style scoped>
+.centered-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: calc(100vh - 64px);
+}
+</style>
+
