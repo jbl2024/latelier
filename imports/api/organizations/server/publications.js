74e214649c1de36d5d771e85a13f016ad808d4bc
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2019-01-15 20:50:07 +0100

    l'admin peut voir toutes les organisations

--- imports/api/organizations/server/publications.js
+++ imports/api/organizations/server/publications.js
@@ -1,11 +1,15 @@
 import { Meteor } from "meteor/meteor";
 import { publishComposite } from "meteor/reywood:publish-composite";
+import { Permissions } from '/imports/api/users/permissions'
 
 import { Organizations } from "../organizations";
 
 Meteor.publish("organizations", function organizations() {
   var userId = Meteor.userId();
-  var query = {$or: [{members: userId}, {isPublic: true}]};
+  let query = {};
+  if (!Permissions.isAdmin(Meteor.userId())) {
+    query['$or'] = [{members: userId}, {isPublic: true}];
+  }
   return Organizations.find(query);
 });
 
