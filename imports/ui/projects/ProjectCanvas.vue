293bda862bbc248b4ded8cb4055b5309d9be63c7
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2019-01-24 21:02:37 +0100

    ajout texte canvas

--- imports/ui/projects/ProjectCanvas.vue
+++ imports/ui/projects/ProjectCanvas.vue
@@ -7,45 +7,123 @@
       <v-container fluid grid-list-md>
         <v-layout row wrap>
           <v-flex xs12 d-flex>
-            <canvas-item title="But" headline="Quelle est l'intention à l'origine du projet ?" :item.sync="data.goal"></canvas-item>
+            <canvas-item title="But" headline="" :item.sync="data.goal">
+              <template slot="headline">
+              Quelle est l'intention à l'origine du projet ? (cause et besoin)<br/>
+              Pourquoi ce projet est-il porteur de sens et important ? et pour qui ? <br/> 
+              Comment ce projet va-t-il changer l'avenir ? et pour qui ?
+              </template>
+            </canvas-item>
           </v-flex>
 
-          <v-flex xs4 d-flex>
-            <canvas-item title="Budget" headline="Quelles sont les finances disponibles ?" :item.sync="data.budget"></canvas-item>
+          <v-flex xs12 sm4 d-flex>
+            <canvas-item title="Budget" headline="Quelles sont les finances disponibles ?" :item.sync="data.budget">
+              <template slot="headline">
+                Quelles sont les finances nécessaires ? disponibles ? <br/>
+                ...pour L‘ÉQUIPE (interne / externe)<br/>
+                ...pour les RESSOURCES
+              </template>
+
+            </canvas-item>
           </v-flex>
-          <v-flex xs4 d-flex>
-            <canvas-item title="Equipe" :item.sync="data.team"></canvas-item>
+          <v-flex xs12 sm4 d-flex>
+            <canvas-item title="Equipe" :item.sync="data.team">
+              <template slot="headline">
+                Qui est / devrait en faire partie ?<br/>
+                ...du noyau de l‘équipe<br/>
+                ...de l‘équipe étendue<br/>
+                ...en tant que partenaires externes<br/>
+                ...en tant que chef de projet / manager
+              </template>
+            </canvas-item>
           </v-flex>
-          <v-flex xs4 d-flex>
-            <canvas-item title="Conditions" :item.sync="data.requirements"></canvas-item>
+          <v-flex xs12 sm4 d-flex>
+            <canvas-item title="Conditions" :item.sync="data.requirements">
+              <template slot="headline">
+              Forces, faits, événements et personnes connues qui influencent le projet ?<br/>
+              ...Qui ou quoi soutient le projet ?<br/>
+              ...Qui ou quoi gênent le projet ?
+              </template>
+            </canvas-item>
           </v-flex>
 
-          <v-flex xs4 d-flex>
-            <canvas-item title="Ressources" :item.sync="data.resources"></canvas-item>
+          <v-flex xs12 sm4 d-flex>
+            <canvas-item title="Ressources" :item.sync="data.resources">
+              <template slot="headline">
+              Quelles sont les ressources nécessaires ? <br/>
+              ...un espace projet<br/>
+              ...des salles de réunions (sur site, à l‘extérieur)<br/>
+              ...des outils (logiciels inclus) et des matériels         
+              </template>
+            </canvas-item>
           </v-flex>
 
-          <v-flex xs4 d-flex>
-            <canvas-item title="Risques & opportunités" :item.sync="data.risks"></canvas-item>
+          <v-flex xs12 sm4 d-flex>
+            <canvas-item title="Risques & opportunités" :item.sync="data.risks">
+              <template slot="headline">
+                  Quels sont les évenements futurs et incertains qui menaceraient ou favoriseraient le projet ?<br/>
+                  Ils vont probablement se concrétiser,ou si vous pouvez les influencer, considérez-les comme des CONDITIONS
+              </template>
+            </canvas-item>
           </v-flex>
 
-          <v-flex xs4 d-flex>
-            <canvas-item title="Jalons" :item.sync="data.milestones"></canvas-item>
+          <v-flex xs12 sm4 d-flex>
+            <canvas-item title="Jalons" :item.sync="data.milestones">
+              <template slot="headline">
+                Quelles sont les étapes clés de progression ?<br/>
+                Les échéances pour :<br/>
+                ...les résultats intermédiaires<br/>
+                ...les décisions cruciales<br/>
+                ...les résultats visibles et mesurables
+              </template>
+            </canvas-item>
           </v-flex>
 
-          <v-flex xs4 d-flex>
-            <canvas-item title="Qualité" :item.sync="data.quality"></canvas-item>
+          <v-flex xs12 sm4 d-flex>
+            <canvas-item title="Qualité" :item.sync="data.quality">
+              <template slot="headline">
+              Qu‘est ce qui rend le CLIENT vraiment heureux au regard :<br/>
+              ...du RÉSULTAT du projet ?<br/>
+              ...des JALONS pour atteindre le résultat ?<br/>
+              ...de la manière d‘être informé / impliqué pendant le projet ?                
+              </template>
+            </canvas-item>
           </v-flex>
 
-          <v-flex xs4 d-flex>
-            <canvas-item title="Résultats" :item.sync="data.outcome"></canvas-item>
+          <v-flex xs12 sm4 d-flex>
+            <canvas-item title="Résultats" :item.sync="data.outcome">
+              <template slot="headline">
+              Qu‘est-ce que le projet est censé livré au CLIENT ? C‘est plutôt :<br/>
+              ...un nouveau produit / service ?<br/>
+              ...une prise de conscience ? <br/>
+              ...de nouvelles connaissances ?             
+              </template>
+            </canvas-item>
           </v-flex>
 
-          <v-flex xs4 d-flex>
-            <canvas-item title="Clients" :item.sync="data.customers"></canvas-item>
+          <v-flex xs12 sm4 d-flex>
+            <canvas-item title="Clients" :item.sync="data.customers">
+              <template slot="headline">
+              Qui est vraiment le client ? Des personnes qui :<br/>
+              ...payent pour le projet (SPONSOR) <br/>
+              ...démarrent & arrêtent le projet (PROPRIÉTAIRE)<br/>
+              ...sont impactées par les résultats (DESTINATAIRES)<br/>
+              S‘il y a plusieurs clients : Y a-t-il des conflits déclarés ?
+              </template>
+            </canvas-item>
           </v-flex>
 
           <v-flex xs12 class="bloc" d-flex>
-            <canvas-item title="Calendrier" :item.sync="data.planning"></canvas-item>
+            <canvas-item title="Calendrier" :item.sync="data.planning">
+              <template slot="headline">
+                Quand le projet démarre-t-il réellement ? <br/>
+                De quoi a-t-on besoin pour cela ? (ex : préparatifs, documents, décisions)<br/>
+                Quand le projet sera-t-il réellemment terminé ? <br/>
+                De quoi a-t-on besoin pour cela ? (ex : documents, décisions)<br/>
+                Quelle souplesse a-t-on sur le déroulement au regard des dates de début et de fin du projet ? <br/>
+                échéances et JALONS compris ?                
+              </template>
+            </canvas-item>
           </v-flex>
         </v-layout>
       </v-container>
