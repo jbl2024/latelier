c0c9c4444b0b1ed137a739a496f984f0b0413424
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2019-03-16 21:39:53 +0100

    tableau de bord : on affiche les projets & orga

--- imports/api/projects/server/publications.js
+++ imports/api/projects/server/publications.js
@@ -2,6 +2,7 @@ import { Meteor } from "meteor/meteor";
 import { publishComposite } from "meteor/reywood:publish-composite";
 
 import { Projects } from "../projects";
+import { Organizations } from "../../organizations/organizations";
 import { ProjectGroups } from "../../projectGroups/projectGroups";
 import { Lists } from "../../lists/lists";
 import { Tasks } from "../../tasks/tasks";
@@ -31,6 +32,58 @@ Meteor.publish("projects", function projectsPublication(organizationId, name, gr
 
   query.organizationId = organizationId;
   return Projects.find(query);
+});
+
+publishComposite("allProjects", (name) => {
+  return {
+    // organizations
+    find() {
+      const userId = Meteor.userId();
+      let query = {};
+      if (!Permissions.isAdmin(userId)) {
+        query['$or'] = [{members: userId}, {isPublic: true}];
+      }
+      return Organizations.find(query);
+    },
+    children: [
+      {
+        // projects
+        find(organization) {
+          const userId = Meteor.userId();
+          let query = {};
+        
+          if (!Permissions.isAdmin(userId)) {
+            query['$or'] = [{createdBy: userId}, {members: userId}, {isPublic: true}];
+          }
+        
+          if (name && name.length > 0) {
+            query['name'] = { $regex: ".*" + name + ".*", $options: "i" };
+          } 
+                
+          query.organizationId = organization._id;
+          return Projects.find(query);
+        },
+        children: [
+          {
+            // users
+            find(project) {
+              var members = project.members || [];
+              return Meteor.users.find(
+                { _id: { $in: members } },
+                { fields: { profile: 1, status: 1, statusDefault: 1, statusConnection: 1, emails: 1 } }
+              );
+            }
+          },
+          {
+            // groups
+            find(project) {
+              return ProjectGroups.find({ organizationId: project.organizationId }, { sort: { name: 1 } });
+            }
+          }
+        ]
+      }
+    ]
+  };
 });
 
 Meteor.publish("projectsForTimeline", function projectsForTimelinePublication(organizationId, name, groupId) {
