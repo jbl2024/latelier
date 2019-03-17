eddff0a1eb97e6f6dd2cba6d720f893b18cf8d1e
Jérôme Blondon <jerome.blondon@ac-lyon.fr>
2019-03-17 08:37:27 +0100

    Affichage mobile specifique pour le dashboard
    On utilise une bottom nav

--- imports/ui/dashboard/DashboardPage.vue
+++ imports/ui/dashboard/DashboardPage.vue
@@ -2,13 +2,91 @@
   <div class="dashboard-page">
     <new-organization ref="newOrganization"></new-organization>
     <template v-if="user">
-      <v-container grid-list-md fluid>
+
+      <div class="show-mobile mobile" v-if="$vuetify.breakpoint.xsOnly">
+        <div class="mobile-organizations" v-if="bottomNav === 'organizations'">
+            <v-card class="flex-container">
+              <v-toolbar dense color="indigo" dark>
+                <v-toolbar-title>{{ $t('Organisations') }}</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn icon @click.stop="newOrganization()">
+                  <v-icon>add</v-icon>
+                </v-btn>
+              </v-toolbar>
+              <dashboard-projects></dashboard-projects>
+            </v-card>
+        </div>
+        <div class="mobile-tasks" v-if="bottomNav === 'tasks'">
+
+            <v-card class="flex-container">
+              <v-toolbar dense color="indigo" dark>
+                <v-toolbar-title>{{ $t('Tasks') }}</v-toolbar-title>
+              </v-toolbar>
+
+              <div class="tabs-wrapper">
+              <v-tabs v-model="tab" icons-and-text centered>
+                <v-tab>
+                  {{ $t('Updated recently') }}
+                  <v-icon>today</v-icon>
+                </v-tab>
+                <v-tab>
+                  {{ $t('Assigned to me') }}
+                  <v-icon>account_circle</v-icon>
+                </v-tab>
+
+                <v-tab>
+                  {{ $t('Late') }}
+                  <v-icon>alarm_on</v-icon>
+                </v-tab>
+
+                <v-tab-item>
+                  <dashboard-task-list :user="user" type="recent"></dashboard-task-list>
+                </v-tab-item>
+                <v-tab-item>
+                  <dashboard-task-list :user="user" type="assignedToMe"></dashboard-task-list>
+                </v-tab-item>
+                <v-tab-item>
+                  <dashboard-task-list :user="user" type="late" empty-illustration="celebration"></dashboard-task-list>
+                </v-tab-item>
+              </v-tabs>
+              </div>
+            </v-card>
+
+        </div>
+          <v-bottom-nav
+            :active.sync="bottomNav"
+            :value="true"
+            absolute
+            dark
+            color="primary"
+          >
+            <v-btn
+              flat
+              value="organizations"
+            >
+              <span>{{ $t('Organizations') }}</span>
+              <v-icon>domain</v-icon>
+            </v-btn>
+
+            <v-btn
+              flat
+              value="tasks"
+            >
+              <span>{{ $t('Tasks') }}</span>
+              <v-icon>list</v-icon>
+            </v-btn>
+
+          </v-bottom-nav>
+      </div>
+
+
+      <v-container grid-list-md fluid class="show-desktop" v-if="$vuetify.breakpoint.smAndUp">
         <v-layout row wrap fill-height>
           <v-flex xs12 md6>
             
             <v-card class="flex-container">
               <v-toolbar dense color="indigo" dark>
-                <v-toolbar-title>{{ $t('Organisations') }}</v-toolbar-title>
+                <v-toolbar-title>{{ $t('Organizations') }}</v-toolbar-title>
                 <v-spacer></v-spacer>
                 <v-btn icon @click.stop="newOrganization()">
                   <v-icon>add</v-icon>
@@ -98,7 +176,8 @@ export default {
       user: null,
       tab: null,
       items: ["web", "shopping", "videos", "images", "news"],
-      text: 'foo'
+      text: 'foo',
+      bottomNav: 'organizations'
     };
   },
   methods: {
@@ -156,6 +235,28 @@ export default {
   right: 0;
   bottom: 0;
   overflow-y: scroll;
+}
+
+@media (max-width: 601px) {
+  .main-container {
+    padding: 0;
+  }
+}
+
+.mobile {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+}
+
+.mobile-organizations {
+  height: calc(100% - 56px);
+}
+
+.mobile-tasks {
+  height: calc(100% - 56px);
 }
 
 </style>
