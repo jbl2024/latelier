8b481888ebaeefb853584b9545297682d4d13b54
Jérôme Blondon <jerome@blondon.fr>
2019-08-13 09:44:42 +0200

    Fix display of weather cards

--- imports/ui/projects/ProjectWeather.vue
+++ imports/ui/projects/ProjectWeather.vue
@@ -155,7 +155,7 @@ export default {
     onResize() {
       const width = this.$refs.container.offsetWidth;
       console.log(width)
-      if (width < 780 && width > 600) {
+      if (width < 780 && !this.$vuetify.breakpoint.xsOnly) {
         this.narrow = true;
       } else {
         this.narrow = false;
