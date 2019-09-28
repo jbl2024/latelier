a960bfc7aaccaf7ddf80b85cf7e2145d4de8cce4
Jérôme Blondon <jerome@blondon.fr>
2019-09-28 13:25:38 +0200

    Remove useless console.log

--- imports/colors.js
+++ imports/colors.js
@@ -24,7 +24,6 @@ export const colors = {
     const isRGB = color.indexOf("rgb") == 0;
     if (isHEX) {
       color = this.fixHex(color);
-      console.log(color)
       let m = color.substr(1).match(color.length == 7 ? /(\S{2})/g : /(\S{1})/g);
       if (m) var r = parseInt(m[0], 16), g = parseInt(m[1], 16), b = parseInt(m[2], 16);
     }
