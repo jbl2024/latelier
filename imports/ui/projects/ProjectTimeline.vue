a4cf3c8f111f17a53cea7a85f1414f8b6717caec
Jérôme Blondon <jerome@blondon.fr>
2019-10-05 13:41:04 +0200

    Improve project (tasks) timeline

--- imports/ui/projects/ProjectTimeline.vue
+++ imports/ui/projects/ProjectTimeline.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="project-timeline">
 
-    <div v-if="!$subReady.project">
+    <template v-if="!$subReady.project">
       <v-progress-linear indeterminate></v-progress-linear>
-    </div>
-    <div v-if="$subReady.project">
-      <div class="progress" v-if="showProgress && count > 0">
-        <v-progress-circular :size="50" color="primary" indeterminate></v-progress-circular>
-      </div>
+    </template>
+    <template v-if="$subReady.project">
       <empty-state v-show="count == 0" icon="mdi-chart-timeline-variant" label="Aucune tache" description="Seules les taches avec une date de début ou de fin sont affichées ici.">
       </empty-state>
 
-      <div class="timeline">
-        <timeline ref="timeline" :items="getItems()" :groups="getGroups()" :options="timeline.options" @changed="onTimelineChanged" @rangechanged="onTimelineRangeChanged" @select="onSelectTask">
+      <v-toolbar dense class="toolbar flex0">
+        <tooltip-button bottom icon="mdi-calendar-today" :tooltip="$t('Today')" @on="gotoToday()"></tooltip-button>
+        <v-divider vertical></v-divider>
+        <tooltip-button bottom icon="mdi-magnify" :tooltip="$t('Reset zoom')" @on="zoomReset()"></tooltip-button>
+        <tooltip-button bottom icon="mdi-magnify-minus" :tooltip="$t('Zoom out')" @on="zoomOut()"></tooltip-button>
+        <tooltip-button bottom icon="mdi-magnify-plus" :tooltip="$t('Zoom in')" @on="zoomIn()"></tooltip-button>
+        <v-divider vertical></v-divider>
+      </v-toolbar>
+
+      <div class="flex1" v-resize="onResizeTimelineContainer" ref="timelineContainer">
+        <timeline ref="timeline" :items="getItems()" :groups="getGroups()" :options="timeline.options" @select="onSelectTask">
         </timeline>
       </div>
 
-    </div>
+    </template>
   </div>
 </template>
 
@@ -24,8 +30,10 @@
 import { Projects } from "/imports/api/projects/projects.js";
 import { Lists } from "/imports/api/lists/lists.js";
 import { Tasks } from "/imports/api/tasks/tasks.js";
-import debounce from "lodash/debounce";
 import { Timeline } from "vue2vis";
+
+import debounce from "lodash/debounce";
+import moment from "moment";
 
 export default {
   components: {
@@ -68,10 +76,22 @@ export default {
             content: "En cours"
           }
         ],
-        options: {}
+        options: {
+          moment: (date) => {
+            if (moment.locale() !== this.$i18n.locale) moment.locale(this.$i18n.locale);
+            return moment(date);
+          },
+          orientation: "top",
+          zoomKey: "ctrlKey",
+          zoomMax: 31556952000 * 3, // 4 years
+          zoomMin: 1 * 1000 * 60 * 60, // 1 hour,
+          // editable: {
+          //   updateTime: true,
+          //   updateGroup: true,
+          //   remove: false
+          // }
+        }
       },
-      showProgress: true,
-      rangeChanged: false
     };
   },
   meteor: {
@@ -132,7 +152,8 @@ export default {
       lists.map(list => {
         const group = {
           id: list._id,
-          content: list.name
+          content: list.name,
+          subgroupStack: true
         };
         groups.push(group);
       });
@@ -146,7 +167,7 @@ export default {
       if (this.project.startDate) {
         items.push({
           id: "start",
-          content: "Début",
+          content: this.getStartContent(),
           start: this.project.startDate,
           type: "box",
           group: 0
@@ -156,7 +177,7 @@ export default {
       if (this.project.endDate) {
         items.push({
           id: "end",
-          content: "Fin",
+          content: this.getEndContent(),
           start: this.project.endDate,
           type: "box",
           group: 0
@@ -178,7 +199,8 @@ export default {
         var item = {
           id: task._id,
           group: task.listId,
-          content: task.name,
+          subgroup: task._id,
+          content: this.getTaskContent(task),
           start: start,
           end: end,
           type: type
@@ -212,15 +234,44 @@ export default {
       return;
     },
 
-    onTimelineChanged() {
-      if (this.rangeChanged) {
-        this.showProgress = false;
-      }
+    gotoToday() {
+      this.$refs.timeline.moveTo(new Date());
     },
 
-    onTimelineRangeChanged() {
-      this.rangeChanged = true;
+    zoomOut() {
+      this.$refs.timeline.zoomOut(0.4);
+    },
+
+    zoomIn() {
+      this.$refs.timeline.zoomIn(0.4);
+    },
+
+    zoomReset() {
+      this.$refs.timeline.fit();
+    },
+
+    onResizeTimelineContainer() {
+      const height = this.$refs.timelineContainer.offsetHeight;
+      this.$refs.timeline.setOptions({
+        height: height
+      });
+    },  
+    
+    getTaskContent(task) {
+      const name = task.name;
+      return `<div class="timeline-custom-item timeline-custom-item-default-colors">${name}</div>`;
+    },
+
+    getStartContent() {
+      const start = this.$t('Start');
+      return `<div class="timeline-custom-item timeline-custom-item-default-colors">${start}</div>`;
+    },
+
+    getEndContent() {
+      const end = this.$t('End');
+      return `<div class="timeline-custom-item timeline-custom-item-default-colors">${end}</div>`;
     }
+    
   }
 };
 </script>
@@ -231,9 +282,20 @@ export default {
   min-height: 0;
   height: 100%;
   flex-direction: column;
-  flex:1;
   position: relative;
+  flex: 1;
+  background-color: white;
 }
+
+.flex0 {
+  flex: 0;
+}
+
+.flex1 {
+  flex: 1;
+  overflow-y: auto;
+}
+
 
 
 .project-timeline {
