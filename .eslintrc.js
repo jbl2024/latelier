9de30ab981a626befddac701f1f8447e61ea6f66
Jérôme Blondon <jerome@blondon.fr>
2019-10-27 14:01:36 +0100

    Fix store

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
-    "extends": [
-      "@meteorjs/eslint-config-meteor",
-      "airbnb-base",
-      'plugin:vue/recommended'
-    ],  
-    rules: {
-      'vue/no-unused-vars': 'off'
-    }
+  extends: [
+    "@meteorjs/eslint-config-meteor",
+    "airbnb-base",
+    "plugin:vue/recommended"
+  ],
+  rules: {
+    "vue/no-unused-vars": "off",
+    "import/no-unresolved": "off",
+    "no-confusing-arrow": "off",
+    "no-param-reassign": "off",
+    "import/no-absolute-path": "off",
+    "quotes": ["error", "double"],
+    "comma-dangle": ["error", "never"]
+  }
 };
