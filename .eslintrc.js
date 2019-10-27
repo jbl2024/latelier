620a4fcf2353dcb30106f019aa38f4f0abfdaf93
Jérôme Blondon <jerome@blondon.fr>
2019-10-27 13:19:21 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": [
+      "@meteorjs/eslint-config-meteor",
+      "airbnb-base",
+      'plugin:vue/recommended'
+    ],  
+    rules: {
+      'vue/no-unused-vars': 'off'
+    }
+};
