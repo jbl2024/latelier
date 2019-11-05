b01d7d3f761789f46d50188b51027a3afd31387f
Jérôme Blondon <jerome@blondon.fr>
2019-11-05 06:39:37 +0100

    Fix errors thrown in console in health report page
    Move methods server side because without publication no healthreport is available on client side

--- imports/api/healthReports/healthReports.js
+++ imports/api/healthReports/healthReports.js
@@ -1,12 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import { Mongo } from "meteor/mongo";
 import HealthReportSchema from "./schema";
-import moment from "moment";
-
-import {
-  checkLoggedIn,
-  checkCanWriteProject
-} from "/imports/api/permissions/permissions";
 
 export const HealthReports = new Mongo.Collection("healthReports");
 HealthReports.attachSchema(HealthReportSchema);
@@ -17,91 +11,3 @@ if (Meteor.isServer) {
     HealthReports.rawCollection().createIndex({ projectId: 1 });
   });
 }
-
-HealthReports.methods.create = new ValidatedMethod({
-  name: "healthReports.create",
-  validate: new SimpleSchema({
-    projectId: { type: String },
-    name: { type: String },
-    description: { type: String },
-    date: { type: String },
-    weather: { type: String }
-  }).validator(),
-  run({ projectId, name, description, date, weather }) {
-    checkLoggedIn();
-    checkCanWriteProject(projectId);
-
-    const convertedDate = moment(date, "YYYY-MM-DD").toDate();
-    const reportId = HealthReports.insert({
-      projectId,
-      name,
-      description,
-      date: convertedDate,
-      weather,
-      createdAt: new Date(),
-      createdBy: Meteor.userId()
-    });
-
-    return reportId;
-  }
-});
-
-HealthReports.methods.update = new ValidatedMethod({
-  name: "healthReports.update",
-  validate: new SimpleSchema({
-    id: { type: String },
-    name: { type: String },
-    description: { type: String, optional: true },
-    date: { type: String },
-    weather: { type: String }
-  }).validator(),
-  run({ id, name, description, date, weather }) {
-    checkLoggedIn();
-
-    const report = HealthReports.findOne({ _id: id });
-    if (!report) {
-      throw new Meteor.Error("not-found");
-    }
-    checkCanWriteProject(report.projectId);
-
-    if (description == null) {
-      description = report.description;
-    }
-
-    const convertedDate = moment(date, "YYYY-MM-DD").toDate();
-    const reportId = HealthReports.update(
-      {
-        _id: id
-      },
-      {
-        $set: {
-          name,
-          description,
-          date: convertedDate,
-          weather,
-          createdAt: new Date(),
-          createdBy: Meteor.userId()
-        }
-      }
-    );
-
-    return reportId;
-  }
-});
-
-HealthReports.methods.remove = new ValidatedMethod({
-  name: "healthReports.remove",
-  validate: new SimpleSchema({
-    id: { type: String }
-  }).validator(),
-  run({ id }) {
-    checkLoggedIn();
-
-    const report = HealthReports.findOne({ _id: id });
-    if (!report) {
-      throw new Meteor.Error("not-found");
-    }
-    checkCanWriteProject(report.projectId);
-    HealthReports.remove(id);
-  }
-});
--- imports/api/healthReports/server/methods.js
+++ imports/api/healthReports/server/methods.js
@@ -5,8 +5,98 @@ import { Projects } from "/imports/api/projects/projects";
 import { Organizations } from "/imports/api/organizations/organizations";
 import {
   checkLoggedIn,
-  checkCanReadProject
+  checkCanReadProject,
+  checkCanWriteProject
 } from "/imports/api/permissions/permissions";
+import moment from "moment";
+
+HealthReports.methods.create = new ValidatedMethod({
+  name: "healthReports.create",
+  validate: new SimpleSchema({
+    projectId: { type: String },
+    name: { type: String },
+    description: { type: String },
+    date: { type: String },
+    weather: { type: String }
+  }).validator(),
+  run({ projectId, name, description, date, weather }) {
+    checkLoggedIn();
+    checkCanWriteProject(projectId);
+
+    const convertedDate = moment(date, "YYYY-MM-DD").toDate();
+    const reportId = HealthReports.insert({
+      projectId,
+      name,
+      description,
+      date: convertedDate,
+      weather,
+      createdAt: new Date(),
+      createdBy: Meteor.userId()
+    });
+
+    return reportId;
+  }
+});
+
+HealthReports.methods.update = new ValidatedMethod({
+  name: "healthReports.update",
+  validate: new SimpleSchema({
+    id: { type: String },
+    name: { type: String },
+    description: { type: String, optional: true },
+    date: { type: String },
+    weather: { type: String }
+  }).validator(),
+  run({ id, name, description, date, weather }) {
+    checkLoggedIn();
+
+    const report = HealthReports.findOne({ _id: id });
+    if (!report) {
+      throw new Meteor.Error("not-found");
+    }
+    checkCanWriteProject(report.projectId);
+
+    if (description == null) {
+      description = report.description;
+    }
+
+    const convertedDate = moment(date, "YYYY-MM-DD").toDate();
+    const reportId = HealthReports.update(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          name,
+          description,
+          date: convertedDate,
+          weather,
+          createdAt: new Date(),
+          createdBy: Meteor.userId()
+        }
+      }
+    );
+
+    return reportId;
+  }
+});
+
+HealthReports.methods.remove = new ValidatedMethod({
+  name: "healthReports.remove",
+  validate: new SimpleSchema({
+    id: { type: String }
+  }).validator(),
+  run({ id }) {
+    checkLoggedIn();
+
+    const report = HealthReports.findOne({ _id: id });
+    if (!report) {
+      throw new Meteor.Error("not-found");
+    }
+    checkCanWriteProject(report.projectId);
+    HealthReports.remove(id);
+  }
+});
 
 HealthReports.methods.findTasks = new ValidatedMethod({
   name: "healthReports.findTasks",
