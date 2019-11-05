3133413582f4fb45a1b028e9a74bbe3ff84428c7
Jérôme Blondon <jerome@blondon.fr>
2019-11-05 22:49:24 +0100

    Solution too slow for quill fix

--- client/components/widgets/RichEditor.vue
+++ client/components/widgets/RichEditor.vue
@@ -1,6 +1,5 @@
 <template>
   <vue-editor
-    :id="id"
     ref="editor"
     v-model="content"
     :class="{ editor: true, 'no-border': noBorder }"
@@ -11,15 +10,6 @@
 
 <script>
 import { VueEditor } from "vue2-editor";
-
-/* eslint-disable */
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-/* eslint-enable */
 
 export default {
   components: {
@@ -41,7 +31,6 @@ export default {
   },
   data() {
     return {
-      id: null,
       content: this.value,
       customToolbar: [
         ["bold", "italic", "underline", "strike"],
@@ -76,7 +65,6 @@ export default {
     }
   },
   mounted() {
-    this.id = uuidv4();
     if (this.autofocus) {
       this.$nextTick(() => {
         this.focus();
