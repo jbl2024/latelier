f670bd41e4839c89480f47a0f6331e41ec00b428
Jérôme Blondon <jerome@blondon.fr>
2020-02-08 22:13:24 +0100

    Avatars are stored on s3 too

--- imports/api/users/avatars.js
+++ imports/api/users/avatars.js
@@ -11,23 +11,48 @@ export const Avatars = new FilesCollection({
   onBeforeUpload() {
     return true;
   },
-  onAfterUpload(file) {
+  onAfterUpload(fileRef) {
     if (Meteor.isServer) {
+      import { onAfterUpload as handleUpload } from "/imports/api/storage/server/upload";
       import { createThumbnails } from "/imports/api/imageProcessing/server/imageProcessing";
 
       this.update(
-        { _id: file._id },
+        { _id: fileRef._id },
         { $set: { "meta.createdAt": new Date() } }
       );
-      if (/png|jpe?g/i.test(file.extension || "")) {
-        createThumbnails(this, file, (error) => {
+      if (/png|jpe?g/i.test(fileRef.extension || "")) {
+        createThumbnails(this, fileRef, (error) => {
           if (error) {
             /* eslint no-console:off */
             console.error(error);
           }
+          handleUpload({
+            collection: Avatars,
+            folder: "avatars",
+            fileRef: fileRef
+          });
+        });
+      } else {
+        handleUpload({
+          collection: Avatars,
+          folder: "avatars",
+          fileRef: fileRef
         });
       }
     }
+  },
+  interceptDownload(http, fileRef, version) {
+    if (Meteor.isServer) {
+      import { interceptDownload as handleDownload } from "/imports/api/storage/server/download";
+
+      return handleDownload({
+        collection: this,
+        http,
+        fileRef,
+        version
+      });
+    }
+    return false;
   }
 });
 Avatars.methods = {};
@@ -36,4 +61,15 @@ if (Meteor.isServer) {
   Meteor.startup(() => {
     Avatars.collection.rawCollection().createIndex({ "meta.userId": 1 });
   });
+
+  // Intercept FilesCollection's remove method to remove file from other storages
+  const _origRemove = Avatars.remove;
+  Avatars.remove = function (search) {
+    import { onBeforeRemove } from "/imports/api/storage/server/remove";
+
+    onBeforeRemove({ collection: this.collection, search });
+
+    // remove original file from database
+    _origRemove.call(this, search);
+  };
 }
