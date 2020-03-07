00414558781e786b85aa4396664f83742598a8d5
Jérôme Blondon <jerome@blondon.fr>
2020-03-07 07:43:45 +0100

    Basic tiptap support

--- client/components/widgets/RichEditor.vue
+++ client/components/widgets/RichEditor.vue
@@ -1,20 +1,22 @@
 <template>
-  <vue-editor
+  <editor-content :editor="editor" />
+  <!-- <vue-editor
     ref="editor"
     v-model="content"
     v-click-outside="onClickOutside"
     :class="{ editor: true, 'no-border': noBorder }"
     :editor-options="editorSettings"
     :editor-toolbar="customToolbar"
-  />
+  /> -->
 </template>
 
 <script>
-import { VueEditor } from "vue2-editor";
+import { Editor, EditorContent } from "tiptap";
+// import { VueEditor } from "vue2-editor";
 
 export default {
   components: {
-    VueEditor
+    EditorContent
   },
   props: {
     value: {
@@ -32,6 +34,7 @@ export default {
   },
   data() {
     return {
+      editor: null,
       content: this.value,
       customToolbar: [
         ["bold", "italic", "underline", "strike"],
@@ -56,25 +59,33 @@ export default {
     };
   },
   watch: {
-    content(content) {
-      this.$emit("input", content);
-    },
     value() {
-      if (this.$refs.editor.quill.root.innerHTML !== this.value) {
-        this.$refs.editor.quill.root.innerHTML = this.value;
+      const value = this.editor.getHTML();
+      if (value !== this.value) {
+        this.editor.setContent(value);
       }
     }
   },
   mounted() {
+    this.editor = new Editor({
+      content: this.content,
+      onUpdate: ({ getHTML }) => {
+        const content = getHTML();
+        this.$emit("input", content);
+      }
+    });
     if (this.autofocus) {
       this.$nextTick(() => {
         this.focus();
       });
     }
   },
+  beforeDestroy() {
+    this.editor.destroy();
+  },
   methods: {
     focus() {
-      this.$refs.editor.quill.focus();
+      this.editor.focus();
     },
     onClickOutside() {
       this.$emit("click-outside");
@@ -82,27 +93,3 @@ export default {
   }
 };
 </script>
-
-<style scoped></style>
-<style>
-.ql-toolbar svg {
-  width: 16px !important;
-  height: 16px !important;
-}
-
-.ql-editor {
-  min-height: 120px !important;
-  font-family: Roboto, sans-serif !important;
-  font-size: 14px !important;
-}
-
-.no-border .ql-toolbar.ql-snow {
-  border-left: none;
-  border-right: none;
-  border-top: none;
-}
-
-.no-border .ql-container.ql-snow {
-  border: none;
-}
-</style>
