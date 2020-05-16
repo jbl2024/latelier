9f07ea55eeb8e6c9ad29f7212151a1273aff3566
Jérôme Blondon <jerome@blondon.fr>
2020-05-16 07:16:16 +0200

    Home go back organization if project is tied to one

--- imports/ui/ui/TopBarTitle.vue
+++ imports/ui/ui/TopBarTitle.vue
@@ -1,6 +1,10 @@
 <template>
-  <project-title v-if="project" :project="project" @go-home="goHome" />
-  <organization-title v-else-if="organization" :organization="organization" @go-home="goHome" />
+  <project-title v-if="project" :project="project" @go-home="goParent" />
+  <organization-title
+    v-else-if="organization"
+    :organization="organization"
+    @go-home="goHome"
+  />
   <home-title v-else-if="!project" @go-home="goHome" />
 </template>
 <script>
@@ -16,6 +20,20 @@ export default {
     }
   },
   methods: {
+    async goParent() {
+      const projectHasOrganization = this.project && this.project.organizationId;
+
+      if (projectHasOrganization) {
+        this.$store.dispatch("project/setCurrentProject", null);
+        await this.$router.push({
+          name: "dashboard-organization-page",
+          params: { organizationId: this.project.organizationId }
+        });
+      } else {
+        this.goHome();
+      }
+    },
+
     async goHome() {
       this.$store.dispatch("organization/setCurrentOrganization", null);
       this.$store.dispatch("project/setCurrentProject", null);
