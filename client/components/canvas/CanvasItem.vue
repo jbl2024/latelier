b03ca12d9899ab906f8eb6966905ca4eb4900814
Jérôme Blondon <jerome@blondon.fr>
2020-06-10 13:45:47 +0200

    Feature/canvas design (#83)
    * Redesign canvas layout
    
    * Remove useless styles
    
    * Static toolbar
    
    * Add missing v-row

--- client/components/canvas/CanvasItem.vue
+++ client/components/canvas/CanvasItem.vue
@@ -1,43 +1,44 @@
 <template>
   <div class="canvas-item">
-    <v-card>
-      <v-card-title primary-title>
-        <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="4">
           <div slot="activator" class="canvas-title">
             {{ title }}
           </div>
           <div class="canvas-headline">
             <slot name="headline" />
           </div>
-        </div>
-      </v-card-title>
-      <v-card-text>
-        <div
-          v-if="!editContent"
-          class="content tiptap-editor-view"
-          @click="startUpdateContent"
-          v-html="linkifyHtml(content)"
-        />
-        <div v-if="editContent" class="edit-content">
-          <rich-editor
-            v-model="content"
-            autofocus
-            @submit="updateContent"
-            @click-outside="updateContent"
+        </v-col>
+        <v-col cols="12" sm="8" class="">
+          <div
+            v-if="!editContent"
+            class="content tiptap-editor-view"
+            @click="startUpdateContent"
+            v-html="linkifyHtml(content)"
           />
-          <v-btn icon text @click.native.stop="updateContent">
-            <v-icon color="green">
-              mdi-check-circle
-            </v-icon>
-          </v-btn>
-          <v-btn icon text @click.native.stop="cancelUpdateContent">
-            <v-icon color="red">
-              mdi-close-circle
-            </v-icon>
-          </v-btn>
-        </div>
-      </v-card-text>
-    </v-card>
+          <div v-if="editContent" class="edit-content">
+            <rich-editor
+              v-model="content"
+              autofocus
+              @submit="updateContent"
+              @click-outside="updateContent"
+            />
+            <v-btn icon text @click.native.stop="updateContent">
+              <v-icon color="green">
+                mdi-check-circle
+              </v-icon>
+            </v-btn>
+            <v-btn icon text @click.native.stop="cancelUpdateContent">
+              <v-icon color="red">
+                mdi-close-circle
+              </v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+      <v-divider class="divider" />
+    </v-container>
   </div>
 </template>
 
@@ -105,12 +106,38 @@ export default {
 }
 
 .content {
-  border: 1px solid #eee;
   width: 100%;
   min-height: 220px;
   cursor: text;
+  background-color: white;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  transition: box-shadow 0.5s ease, opacity 0.5s ease,
+    background-color 0.5s ease;
+  padding: 24px;
+  font-size: 16px;
 }
+
+.content:hover {
+  box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.25);
+  cursor: text;
+}
+
 .edit-content {
   min-height: 220px;
+  width: 100%;
+  min-height: 220px;
+  cursor: text;
+  background-color: white;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  transition: box-shadow 0.5s ease, opacity 0.5s ease,
+    background-color 0.5s ease;
+   padding: 12px;
+  font-size: 16px;
+}
+
+.divider {
+  margin-top: 50px;
 }
 </style>
--- imports/ui/projects/ProjectCanvas.vue
+++ imports/ui/projects/ProjectCanvas.vue
@@ -4,8 +4,8 @@
       <v-progress-linear indeterminate />
     </div>
     <div v-else class="wrapper">
-      <v-toolbar dense class="toolbar">
-        <span class="title">{{ $t('Canvas') }}</span>
+      <v-toolbar dense class="flex0">
+        <span class="title">{{ $t("Canvas") }}</span>
         <tooltip-button
           bottom
           icon="mdi-file-export"
@@ -13,184 +13,185 @@
           @on="exportODT()"
         />
       </v-toolbar>
-      <v-container fluid grid-list-md class="canvas">
-        <v-layout row wrap>
-          <v-flex xs12>
-            <canvas-item
-              flex-grow-1
-              title="But"
-              headline=""
-              :item.sync="canvas.data.goal"
-              @save="save()"
-            >
-              <template slot="headline">
-                Quelle est l'intention à l'origine du projet ? (cause et
-                besoin)<br>
-                Pourquoi ce projet est-il porteur de sens et important ? et
-                pour qui ? <br>
-                Comment ce projet va-t-il changer l'avenir ? et pour qui ?
-              </template>
-            </canvas-item>
-          </v-flex>
+      <div class="flex1">
+        <v-container fluid>
+          <v-row>
+            <v-col cols="12">
+              <canvas-item
+                title="But"
+                headline=""
+                :item.sync="canvas.data.goal"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Quelle est l'intention à l'origine du projet ? (cause et
+                  besoin)<br>
+                  Pourquoi ce projet est-il porteur de sens et important ? et
+                  pour qui ? <br>
+                  Comment ce projet va-t-il changer l'avenir ? et pour qui ?
+                </template>
+              </canvas-item>
+            </v-col>
 
-          <v-flex xs12 sm4>
-            <canvas-item
-              title="Budget"
-              headline="Quelles sont les finances disponibles ?"
-              :item.sync="canvas.data.budget"
-              @save="save()"
-            >
-              <template slot="headline">
-                Quelles sont les finances nécessaires ? disponibles ? <br>
-                ...pour L‘ÉQUIPE (interne / externe)<br>
-                ...pour les RESSOURCES
-              </template>
-            </canvas-item>
-          </v-flex>
-          <v-flex xs12 sm4>
-            <canvas-item
-              title="Equipe"
-              :item.sync="canvas.data.team"
-              @save="save()"
-            >
-              <template slot="headline">
-                Qui est / devrait en faire partie ?<br>
-                ...du noyau de l‘équipe<br>
-                ...de l‘équipe étendue<br>
-                ...en tant que partenaires externes<br>
-                ...en tant que chef de projet / manager
-              </template>
-            </canvas-item>
-          </v-flex>
-          <v-flex xs12 sm4>
-            <canvas-item
-              title="Conditions"
-              :item.sync="canvas.data.requirements"
-              @save="save()"
-            >
-              <template slot="headline">
-                Forces, faits, événements et personnes connues qui influencent
-                le projet ?<br>
-                ...Qui ou quoi soutient le projet ?<br>
-                ...Qui ou quoi gênent le projet ?
-              </template>
-            </canvas-item>
-          </v-flex>
+            <v-col cols="12">
+              <canvas-item
+                title="Budget"
+                headline="Quelles sont les finances disponibles ?"
+                :item.sync="canvas.data.budget"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Quelles sont les finances nécessaires ? disponibles ? <br>
+                  ...pour L‘ÉQUIPE (interne / externe)<br>
+                  ...pour les RESSOURCES
+                </template>
+              </canvas-item>
+            </v-col>
+            <v-col cols="12">
+              <canvas-item
+                title="Equipe"
+                :item.sync="canvas.data.team"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Qui est / devrait en faire partie ?<br>
+                  ...du noyau de l‘équipe<br>
+                  ...de l‘équipe étendue<br>
+                  ...en tant que partenaires externes<br>
+                  ...en tant que chef de projet / manager
+                </template>
+              </canvas-item>
+            </v-col>
+            <v-col cols="12">
+              <canvas-item
+                title="Conditions"
+                :item.sync="canvas.data.requirements"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Forces, faits, événements et personnes connues qui influencent
+                  le projet ?<br>
+                  ...Qui ou quoi soutient le projet ?<br>
+                  ...Qui ou quoi gênent le projet ?
+                </template>
+              </canvas-item>
+            </v-col>
 
-          <v-flex xs12 sm4>
-            <canvas-item
-              title="Ressources"
-              :item.sync="canvas.data.resources"
-              @save="save()"
-            >
-              <template slot="headline">
-                Quelles sont les ressources nécessaires ? <br>
-                ...un espace projet<br>
-                ...des salles de réunions (sur site, à l‘extérieur)<br>
-                ...des outils (logiciels inclus) et des matériels
-              </template>
-            </canvas-item>
-          </v-flex>
+            <v-col cols="12">
+              <canvas-item
+                title="Ressources"
+                :item.sync="canvas.data.resources"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Quelles sont les ressources nécessaires ? <br>
+                  ...un espace projet<br>
+                  ...des salles de réunions (sur site, à l‘extérieur)<br>
+                  ...des outils (logiciels inclus) et des matériels
+                </template>
+              </canvas-item>
+            </v-col>
 
-          <v-flex xs12 sm4>
-            <canvas-item
-              title="Risques & opportunités"
-              :item.sync="canvas.data.risks"
-              @save="save()"
-            >
-              <template slot="headline">
-                Quels sont les évenements futurs et incertains qui
-                menaceraient ou favoriseraient le projet ?<br>
-                Ils vont probablement se concrétiser,ou si vous pouvez les
-                influencer, considérez-les comme des CONDITIONS
-              </template>
-            </canvas-item>
-          </v-flex>
+            <v-col cols="12">
+              <canvas-item
+                title="Risques & opportunités"
+                :item.sync="canvas.data.risks"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Quels sont les évenements futurs et incertains qui
+                  menaceraient ou favoriseraient le projet ?<br>
+                  Ils vont probablement se concrétiser,ou si vous pouvez les
+                  influencer, considérez-les comme des CONDITIONS
+                </template>
+              </canvas-item>
+            </v-col>
 
-          <v-flex xs12 sm4>
-            <canvas-item
-              title="Jalons"
-              :item.sync="canvas.data.milestones"
-              @save="save()"
-            >
-              <template slot="headline">
-                Quelles sont les étapes clés de progression ?<br>
-                Les échéances pour :<br>
-                ...les résultats intermédiaires<br>
-                ...les décisions cruciales<br>
-                ...les résultats visibles et mesurables
-              </template>
-            </canvas-item>
-          </v-flex>
+            <v-col cols="12">
+              <canvas-item
+                title="Jalons"
+                :item.sync="canvas.data.milestones"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Quelles sont les étapes clés de progression ?<br>
+                  Les échéances pour :<br>
+                  ...les résultats intermédiaires<br>
+                  ...les décisions cruciales<br>
+                  ...les résultats visibles et mesurables
+                </template>
+              </canvas-item>
+            </v-col>
 
-          <v-flex xs12 sm4>
-            <canvas-item
-              title="Qualité"
-              :item.sync="canvas.data.quality"
-              @save="save()"
-            >
-              <template slot="headline">
-                Qu‘est ce qui rend le CLIENT vraiment heureux au regard :<br>
-                ...du RÉSULTAT du projet ?<br>
-                ...des JALONS pour atteindre le résultat ?<br>
-                ...de la manière d‘être informé / impliqué pendant le projet ?
-              </template>
-            </canvas-item>
-          </v-flex>
+            <v-col cols="12">
+              <canvas-item
+                title="Qualité"
+                :item.sync="canvas.data.quality"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Qu‘est ce qui rend le CLIENT vraiment heureux au regard :<br>
+                  ...du RÉSULTAT du projet ?<br>
+                  ...des JALONS pour atteindre le résultat ?<br>
+                  ...de la manière d‘être informé / impliqué pendant le projet ?
+                </template>
+              </canvas-item>
+            </v-col>
 
-          <v-flex xs12 sm4>
-            <canvas-item
-              title="Résultats"
-              :item.sync="canvas.data.outcome"
-              @save="save()"
-            >
-              <template slot="headline">
-                Qu‘est-ce que le projet est censé livré au CLIENT ? C‘est
-                plutôt :<br>
-                ...un nouveau produit / service ?<br>
-                ...une prise de conscience ? <br>
-                ...de nouvelles connaissances ?
-              </template>
-            </canvas-item>
-          </v-flex>
+            <v-col cols="12">
+              <canvas-item
+                title="Résultats"
+                :item.sync="canvas.data.outcome"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Qu‘est-ce que le projet est censé livré au CLIENT ? C‘est
+                  plutôt :<br>
+                  ...un nouveau produit / service ?<br>
+                  ...une prise de conscience ? <br>
+                  ...de nouvelles connaissances ?
+                </template>
+              </canvas-item>
+            </v-col>
 
-          <v-flex xs12 sm4>
-            <canvas-item
-              title="Clients"
-              :item.sync="canvas.data.customers"
-              @save="save()"
-            >
-              <template slot="headline">
-                Qui est vraiment le client ? Des personnes qui :<br>
-                ...payent pour le projet (SPONSOR) <br>
-                ...démarrent & arrêtent le projet (PROPRIÉTAIRE)<br>
-                ...sont impactées par les résultats (DESTINATAIRES)<br>
-                S‘il y a plusieurs clients : Y a-t-il des conflits déclarés ?
-              </template>
-            </canvas-item>
-          </v-flex>
+            <v-col cols="12">
+              <canvas-item
+                title="Clients"
+                :item.sync="canvas.data.customers"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Qui est vraiment le client ? Des personnes qui :<br>
+                  ...payent pour le projet (SPONSOR) <br>
+                  ...démarrent & arrêtent le projet (PROPRIÉTAIRE)<br>
+                  ...sont impactées par les résultats (DESTINATAIRES)<br>
+                  S‘il y a plusieurs clients : Y a-t-il des conflits déclarés ?
+                </template>
+              </canvas-item>
+            </v-col>
 
-          <v-flex xs12 class="bloc">
-            <canvas-item
-              title="Calendrier"
-              :item.sync="canvas.data.planning"
-              @save="save()"
-            >
-              <template slot="headline">
-                Quand le projet démarre-t-il réellement ? <br>
-                De quoi a-t-on besoin pour cela ? (ex : préparatifs,
-                documents, décisions)<br>
-                Quand le projet sera-t-il réellemment terminé ? <br>
-                De quoi a-t-on besoin pour cela ? (ex : documents,
-                décisions)<br>
-                Quelle souplesse a-t-on sur le déroulement au regard des dates
-                de début et de fin du projet ? <br>
-                échéances et JALONS compris ?
-              </template>
-            </canvas-item>
-          </v-flex>
-        </v-layout>
-      </v-container>
+            <v-col cols="12" class="bloc">
+              <canvas-item
+                title="Calendrier"
+                :item.sync="canvas.data.planning"
+                @save="save()"
+              >
+                <template slot="headline">
+                  Quand le projet démarre-t-il réellement ? <br>
+                  De quoi a-t-on besoin pour cela ? (ex : préparatifs,
+                  documents, décisions)<br>
+                  Quand le projet sera-t-il réellemment terminé ? <br>
+                  De quoi a-t-on besoin pour cela ? (ex : documents,
+                  décisions)<br>
+                  Quelle souplesse a-t-on sur le déroulement au regard des dates
+                  de début et de fin du projet ? <br>
+                  échéances et JALONS compris ?
+                </template>
+              </canvas-item>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
     </div>
   </div>
 </template>
@@ -210,10 +211,12 @@ export default {
   },
   computed: {
     isReady() {
-      return this.$subReady.canvas
-      && this.canvas
-      && this.canvas.data
-      && this.currentProject;
+      return (
+        this.$subReady.canvas
+        && this.canvas
+        && this.canvas.data
+        && this.currentProject
+      );
     },
     ...mapState("project", ["currentProject"])
   },
@@ -229,7 +232,7 @@ export default {
       project() {
         return [this.projectId];
       },
-      canvas: function() {
+      canvas: function () {
         return [this.projectId];
       }
     },
@@ -250,14 +253,20 @@ export default {
     },
 
     exportODT() {
-      Meteor.call("canvas.exportODT", { projectId: this.projectId }, (error, result) => {
-        if (error) {
-          this.$notifyError(error);
-          return;
+      Meteor.call(
+        "canvas.exportODT",
+        { projectId: this.projectId },
+        (error, result) => {
+          if (error) {
+            this.$notifyError(error);
+            return;
+          }
+          const blob = new Blob([result.data], {
+            type: "application/vnd.oasis.opendocument.text"
+          });
+          saveAs(blob, "canvas.odt");
         }
-        const blob = new Blob([result.data], { type: "application/vnd.oasis.opendocument.text" });
-        saveAs(blob, "canvas.odt");
-      });
+      );
     }
   }
 };
@@ -267,15 +276,18 @@ export default {
 .wrapper {
   display: flex;
   flex-direction: column;
+  position: absolute;
+  height: 100%;
+  width: 100%;
 }
 
-.toolbar {
+.flex0 {
   flex: 0;
+  height: 100%;
 }
 
-.canvas {
-  flex: 1;
-  overflow-y: auto;
-  margin-top: 4px;
+.flex1 {
+  flex: 1; /* takes the remaining height of the "container" div */
+  overflow: auto; /* to scroll just the "main" div */
 }
 </style>
