52eca8d225da54147756e261ff60f6f36b47c45a
Jérôme Blondon <jerome@blondon.fr>
2020-06-14 16:52:39 +0200

    Rich editor: add component for floating menu

--- client/components/widgets/richEditor/RichEditor.vue
+++ client/components/widgets/richEditor/RichEditor.vue
@@ -9,127 +9,8 @@
     @click="focus"
   >
     <rich-editor-menu-bubble :editor="editor" />
-
-    <editor-floating-menu
-      v-if="floating"
-      v-slot="{ commands, isActive, menu }"
-      :editor="editor"
-    >
-      <div
-        class="editor__floating-menu"
-        :class="{ 'is-active': menu.isActive }"
-        :style="`top: ${menu.top}px`"
-      >
-        <v-btn
-          v-if="!expandFloatingToolbar && menu.isActive"
-          color="indigo"
-          fab
-          outlined
-          x-small
-          class="xx-small"
-          dark
-          @click="expandFloatingToolbar = true"
-        >
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-        <template v-if="expandFloatingToolbar">
-          <v-btn
-            color="indigo"
-            fab
-            x-small
-            class="xx-small"
-            dark
-            @click="expandFloatingToolbar = false"
-          >
-            <v-icon>mdi-minus</v-icon>
-          </v-btn>
-          <button @click="commands.heading({ level: 1 })">
-            <v-icon
-              class="small-medium"
-              :color="iconColor(isActive.heading({ level: 1 }))"
-            >
-              mdi-format-header-1
-            </v-icon>
-          </button>
-          <button @click="commands.heading({ level: 2 })">
-            <v-icon
-              class="small-medium"
-              :color="iconColor(isActive.heading({ level: 2 }))"
-            >
-              mdi-format-header-2
-            </v-icon>
-          </button>
-          <button @click="commands.heading({ level: 3 })">
-            <v-icon
-              class="small-medium"
-              :color="iconColor(isActive.heading({ level: 3 }))"
-            >
-              mdi-format-header-3
-            </v-icon>
-          </button>
-          <button @click="commands.bold">
-            <v-icon class="small-medium" :color="iconColor(isActive.bold())">
-              mdi-format-bold
-            </v-icon>
-          </button>
-          <button @click="commands.italic">
-            <v-icon class="small-medium" :color="iconColor(isActive.italic())">
-              mdi-format-italic
-            </v-icon>
-          </button>
-          <button @click="commands.underline">
-            <v-icon
-              class="small-medium"
-              :color="iconColor(isActive.underline())"
-            >
-              mdi-format-underline
-            </v-icon>
-          </button>
-          <button @click="commands.strike">
-            <v-icon class="small-medium" :color="iconColor(isActive.strike())">
-              mdi-format-strikethrough
-            </v-icon>
-          </button>
-          <button @click="commands.ordered_list">
-            <v-icon
-              class="small-medium"
-              :color="iconColor(isActive.ordered_list())"
-            >
-              mdi-format-list-numbered
-            </v-icon>
-          </button>
-          <button @click="commands.bullet_list">
-            <v-icon
-              class="small-medium"
-              :color="iconColor(isActive.bullet_list())"
-            >
-              mdi-format-list-bulleted
-            </v-icon>
-          </button>
-          <button @click="commands.code_block">
-            <v-icon
-              class="small-medium"
-              :color="iconColor(isActive.code_block())"
-            >
-              mdi-xml
-            </v-icon>
-          </button>
-          <button @click="commands.blockquote">
-            <v-icon
-              class="small-medium"
-              :color="iconColor(isActive.blockquote())"
-            >
-              mdi-format-quote-close
-            </v-icon>
-          </button>
-        </template>
-      </div>
-    </editor-floating-menu>
-
-    <rich-editor-menu-bar
-      v-if="!hideToolbar"
-      :editor="editor"
-    />
+    <rich-editor-floating-menu v-if="floating" menu :editor="editor" />
+    <rich-editor-menu-bar v-if="!hideToolbar" :editor="editor" />
 
     <editor-content
       :editor="editor"
@@ -144,11 +25,7 @@
 </template>
 
 <script>
-import {
-  Editor,
-  EditorContent,
-  EditorFloatingMenu
-} from "tiptap";
+import { Editor, EditorContent } from "tiptap";
 import {
   Blockquote,
   CodeBlock,
@@ -167,8 +44,7 @@ import {
 
 export default {
   components: {
-    EditorContent,
-    EditorFloatingMenu
+    EditorContent
   },
   props: {
     value: {
@@ -205,8 +81,7 @@ export default {
       editor: null,
       content: this.value,
       ctrl: false,
-      enter: false,
-      expandFloatingToolbar: false
+      enter: false
     };
   },
   computed: {
