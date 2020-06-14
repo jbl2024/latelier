fee278a44f1251bb718be79f48dd393af7e1c422
Jérôme Blondon <jerome@blondon.fr>
2020-06-14 16:04:48 +0200

    Rich editor: add component for menu bar

--- client/components/widgets/richEditor/RichEditor.vue
+++ client/components/widgets/richEditor/RichEditor.vue
@@ -8,7 +8,7 @@
     }"
     @click="focus"
   >
-    <rich-editor-bubble-menu :editor="editor" />
+    <rich-editor-menu-bubble :editor="editor" />
 
     <editor-floating-menu
       v-if="floating"
@@ -126,90 +126,11 @@
       </div>
     </editor-floating-menu>
 
-    <editor-menu-bar
+    <rich-editor-menu-bar
       v-if="!hideToolbar"
-      v-slot="{ commands, isActive, focused }"
       :editor="editor"
-    >
-      <div class="menubar top-toolbar" :class="{ 'is-focused': focused }">
-        <button @click="commands.bold">
-          <v-icon class="small-medium" :color="iconColor(isActive.bold())">
-            mdi-format-bold
-          </v-icon>
-        </button>
-        <button @click="commands.italic">
-          <v-icon class="small-medium" :color="iconColor(isActive.italic())">
-            mdi-format-italic
-          </v-icon>
-        </button>
-        <button @click="commands.underline">
-          <v-icon class="small-medium" :color="iconColor(isActive.underline())">
-            mdi-format-underline
-          </v-icon>
-        </button>
-        <button @click="commands.strike">
-          <v-icon class="small-medium" :color="iconColor(isActive.strike())">
-            mdi-format-strikethrough
-          </v-icon>
-        </button>
-        <button @click="commands.ordered_list">
-          <v-icon
-            class="small-medium"
-            :color="iconColor(isActive.ordered_list())"
-          >
-            mdi-format-list-numbered
-          </v-icon>
-        </button>
-        <button @click="commands.bullet_list">
-          <v-icon
-            class="small-medium"
-            :color="iconColor(isActive.bullet_list())"
-          >
-            mdi-format-list-bulleted
-          </v-icon>
-        </button>
-        <button @click="commands.code_block">
-          <v-icon
-            class="small-medium"
-            :color="iconColor(isActive.code_block())"
-          >
-            mdi-xml
-          </v-icon>
-        </button>
-        <button @click="commands.blockquote">
-          <v-icon
-            class="small-medium"
-            :color="iconColor(isActive.blockquote())"
-          >
-            mdi-format-quote-close
-          </v-icon>
-        </button>
-        <button @click="commands.heading({ level: 1 })">
-          <v-icon
-            class="small-medium"
-            :color="iconColor(isActive.heading({ level: 1 }))"
-          >
-            mdi-format-header-1
-          </v-icon>
-        </button>
-        <button @click="commands.heading({ level: 2 })">
-          <v-icon
-            class="small-medium"
-            :color="iconColor(isActive.heading({ level: 2 }))"
-          >
-            mdi-format-header-2
-          </v-icon>
-        </button>
-        <button @click="commands.heading({ level: 3 })">
-          <v-icon
-            class="small-medium"
-            :color="iconColor(isActive.heading({ level: 3 }))"
-          >
-            mdi-format-header-3
-          </v-icon>
-        </button>
-      </div>
-    </editor-menu-bar>
+    />
+
     <editor-content
       :editor="editor"
       :class="{
@@ -226,7 +147,6 @@
 import {
   Editor,
   EditorContent,
-  EditorMenuBar,
   EditorFloatingMenu
 } from "tiptap";
 import {
@@ -248,7 +168,6 @@ import {
 export default {
   components: {
     EditorContent,
-    EditorMenuBar,
     EditorFloatingMenu
   },
   props: {
---
+++ client/components/widgets/richEditor/RichEditorMenuBar.vue
@@ -0,0 +1,159 @@
+<template>
+  <editor-menu-bar
+    v-slot="{ commands, isActive, focused }"
+    :editor="editor"
+  >
+    <div class="menubar top-toolbar" :class="{ 'is-focused': focused }">
+      <button @click="commands.bold">
+        <v-icon class="small-medium" :color="iconColor(isActive.bold())">
+          mdi-format-bold
+        </v-icon>
+      </button>
+      <button @click="commands.italic">
+        <v-icon class="small-medium" :color="iconColor(isActive.italic())">
+          mdi-format-italic
+        </v-icon>
+      </button>
+      <button @click="commands.underline">
+        <v-icon class="small-medium" :color="iconColor(isActive.underline())">
+          mdi-format-underline
+        </v-icon>
+      </button>
+      <button @click="commands.strike">
+        <v-icon class="small-medium" :color="iconColor(isActive.strike())">
+          mdi-format-strikethrough
+        </v-icon>
+      </button>
+      <button @click="commands.ordered_list">
+        <v-icon
+          class="small-medium"
+          :color="iconColor(isActive.ordered_list())"
+        >
+          mdi-format-list-numbered
+        </v-icon>
+      </button>
+      <button @click="commands.bullet_list">
+        <v-icon class="small-medium" :color="iconColor(isActive.bullet_list())">
+          mdi-format-list-bulleted
+        </v-icon>
+      </button>
+      <button @click="commands.code_block">
+        <v-icon class="small-medium" :color="iconColor(isActive.code_block())">
+          mdi-xml
+        </v-icon>
+      </button>
+      <button @click="commands.blockquote">
+        <v-icon class="small-medium" :color="iconColor(isActive.blockquote())">
+          mdi-format-quote-close
+        </v-icon>
+      </button>
+      <button @click="commands.heading({ level: 1 })">
+        <v-icon
+          class="small-medium"
+          :color="iconColor(isActive.heading({ level: 1 }))"
+        >
+          mdi-format-header-1
+        </v-icon>
+      </button>
+      <button @click="commands.heading({ level: 2 })">
+        <v-icon
+          class="small-medium"
+          :color="iconColor(isActive.heading({ level: 2 }))"
+        >
+          mdi-format-header-2
+        </v-icon>
+      </button>
+      <button @click="commands.heading({ level: 3 })">
+        <v-icon
+          class="small-medium"
+          :color="iconColor(isActive.heading({ level: 3 }))"
+        >
+          mdi-format-header-3
+        </v-icon>
+      </button>
+    </div>
+  </editor-menu-bar>
+</template>
+
+<script>
+import { EditorMenuBar } from "tiptap";
+
+export default {
+  components: {
+    EditorMenuBar
+  },
+  props: {
+    editor: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {};
+  },
+
+  methods: {
+    iconColor(activated) {
+      if (activated) {
+        return "blue";
+      }
+      return "primary";
+    },
+
+    iconColorDark(activated) {
+      if (activated) {
+        return "blue";
+      }
+      return "white";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.menubar {
+  border-bottom: 1px solid #ccc;
+  padding: 8px;
+  margin-bottom: 0.5rem;
+  transition: visibility 0.2s 0.4s, opacity 0.2s 0.4s;
+
+  &.is-hidden {
+    display: none;
+    opacity: 0;
+  }
+
+  &.is-focused {
+    display: block;
+    opacity: 1;
+    transition: visibility 0.2s, opacity 0.2s;
+  }
+
+  &__button {
+    font-weight: bold;
+    display: inline-flex;
+    background: transparent;
+    border: 0;
+    color: black;
+    padding: 0.2rem 0.5rem;
+    margin-right: 0.2rem;
+    border-radius: 3px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: rgba(black, 0.05);
+    }
+
+    &.is-active {
+      background-color: rgba(black, 0.1);
+    }
+  }
+
+  span#{&}__button {
+    font-size: 13.3333px;
+  }
+}
+
+.small-medium {
+  font-size: 18px !important;
+}
+</style>
