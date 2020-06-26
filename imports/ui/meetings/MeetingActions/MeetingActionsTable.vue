4716a39aae64442a4212459f6224f88018b0694a
Jérôme Blondon <jerome@blondon.fr>
2020-06-26 22:39:23 +0200

    i18n

--- imports/ui/meetings/MeetingActions/MeetingActionsTable.vue
+++ imports/ui/meetings/MeetingActions/MeetingActionsTable.vue
@@ -15,7 +15,7 @@
               <v-icon left>
                 mdi-plus
               </v-icon>
-              Ajouter une décision
+              {{ $t('meetings.actions.addAction') }}
             </v-btn>
           </v-toolbar-title>
           <v-spacer />
