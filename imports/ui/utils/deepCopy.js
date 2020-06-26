257d9d894d8415cad786ea02f5fdf9047fe87dad
Jérôme Blondon <jerome@blondon.fr>
2020-06-26 23:51:36 +0200

    Deepcopy: add support for date

--- imports/ui/utils/deepCopy.js
+++ imports/ui/utils/deepCopy.js
@@ -4,7 +4,9 @@ export default function deepCopy(src) {
   for (const key in src) {
     const v = src[key];
     if (v) {
-      if (typeof v === "object") {
+      if (typeof v.getMonth === 'function') {
+        target[key] = new Date(v.valueOf());
+      } else if (typeof v === "object") {
         target[key] = deepCopy(v);
       } else {
         target[key] = v;
