3d37a5ec9f9c41f3f2fe6d67195291fdcd1d91a0
mbenzerga <mbenzerga@CZC6498DRM.IN-AC-LYON>
2020-10-09 10:44:21 +0200

    List all API routes on /api/routes only in dev mode

--- imports/api/routes.js
+++ imports/api/routes.js
@@ -3,6 +3,7 @@ import projects from "/imports/api/projects/server/rest.js";
 import tasks from "/imports/api/tasks/server/rest.js";
 import organizations from "/imports/api/organizations/server/rest.js";
 import users from "/imports/api/users/server/rest.js";
+import listEndpoints from "express-list-endpoints";
 
 const routes = express.Router();
 
@@ -10,5 +11,15 @@ routes.use("/projects", projects);
 routes.use("/tasks", tasks);
 routes.use("/organizations", organizations);
 routes.use("/users", users);
+
+// Listing all API Routes
+if (Meteor.isDevelopment) {
+    routes.get("/routes", function(req, res) {
+        res.status(200).json(listEndpoints(routes).map(endpoint => {
+            if (endpoint.middleware) delete endpoint.middleware;
+            return endpoint;
+        }));
+    });
+}
 
 export default routes;
