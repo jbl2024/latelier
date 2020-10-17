d3775b5a64e23e56ba9929ef6f8b176f01d70973
Jérôme Blondon <jerome@blondon.fr>
2020-10-17 08:13:46 +0200

    Feature/improve meeting calendar (#100)
    * Add drag create event, remove right mouse button
    
    * Fix drag outside

--- imports/ui/meetings/MeetingCalendar/MeetingCalendar.vue
+++ imports/ui/meetings/MeetingCalendar/MeetingCalendar.vue
@@ -1,18 +1,5 @@
 <template>
   <div>
-    <meeting-calendar-contextual-menu
-      v-model="showContextualMenu"
-      :x="x"
-      :y="y"
-    >
-      <v-list>
-        <v-list-item @click="addNewMeetingOnTime">
-          <v-list-item-title>
-            {{ addMeetingText }}
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </meeting-calendar-contextual-menu>
     <v-calendar
       ref="calendar"
       v-model="selectedDate"
@@ -23,7 +10,7 @@
       :min-weeks="minWeeks"
       :max-days="calendarDatas.maxDays"
       :now="now"
-      :events="events"
+      :events="localEvents"
       event-overlap-mode="stack"
       :interval-height="intervalHeight"
       :event-color="getEventColor"
@@ -32,9 +19,13 @@
       :short-months="false"
       :interval-count="24 - firstInterval"
       :first-interval="firstInterval"
-      @contextmenu:time="showContextMenu"
       @click:event="selectEvent"
       @change="changeCalendar"
+      @mousedown:event="startDrag"
+      @mousedown:time="startTime"
+      @mousemove:time="mouseMove"
+      @mouseup:time="endDrag"
+      @mouseleave.native="cancelDrag"
     >
       <template #event="{ event }">
         <meeting-calendar-event
@@ -49,7 +40,6 @@
 </template>
 <script>
 import DatesMixin from "/imports/ui/mixins/DatesMixin.js";
-import MeetingCalendarContextualMenu from "/imports/ui/meetings/MeetingCalendar/MeetingCalendarContextualMenu";
 import MeetingCalendarEvent from "/imports/ui/meetings/MeetingCalendar/MeetingCalendarEvent";
 import moment from "moment";
 
@@ -57,7 +47,6 @@ const weekdaysDefault = [1, 2, 3, 4, 5, 6, 0];
 const fiveWeekdays = [1, 2, 3, 4, 5];
 export default {
   components: {
-    MeetingCalendarContextualMenu,
     MeetingCalendarEvent
   },
   mixins: [DatesMixin],
@@ -101,12 +90,12 @@ export default {
   },
   data() {
     return {
-      nowMenu: false,
-      showContextualMenu: false,
-      clickedTime: null,
-      x: 0,
-      y: 0,
-      minWeeks: 1
+      minWeeks: 1,
+      dragEvent: null,
+      dragStart: null,
+      createEvent: null,
+      createStart: null,
+      localEvents: []
     };
   },
   computed: {
@@ -131,10 +120,6 @@ export default {
     },
     selectedDayNumber() {
       return moment(this.selectedDate).format("d");
-    },
-    addMeetingText() {
-      const hour = this.clickedTime?.time ? this.clickedTime.time.split(":")[0] : "";
-      return `Ajouter une réunion ${hour ? (`à ${hour}h`) : ""}`;
     },
     calendarDatas() {
       switch (this.selectedDisplayType) {
@@ -163,9 +148,18 @@ export default {
     }
   },
   watch: {
+    events: {
+      immediate: true,
+      deep: true,
+      handler(events) {
+        this.localEvents = Array.from(events);
+      }
+    },
     selectedDayNumber() {
-      if (this.displayType === "5days"
-      && !fiveWeekdays.includes(parseInt(this.selectedDayNumber, 10))) {
+      if (
+        this.displayType === "5days"
+        && !fiveWeekdays.includes(parseInt(this.selectedDayNumber, 10))
+      ) {
         this.selectedDisplayType = "week";
       }
     }
@@ -196,36 +190,124 @@ export default {
       return event.color;
     },
     getEventName({ input }) {
-      return `${input.name} (${input?.project?.name ? input.project.name : ""})`;
+      return `${input.name} (${
+        input?.project?.name ? input.project.name : ""
+      })`;
     },
     showContextMenu(data) {
       this.clickedTime = data;
       this.showContextualMenu = true;
     },
-    addNewMeetingOnTime() {
-      this.$emit("add-new-meeting", this.clickedTime);
-    },
     clickHandler(event) {
       this.x = event.clientX;
       this.y = event.clientY;
+    },
+
+    startDrag({ event, timed }) {
+      if (event && timed) {
+        this.dragEvent = event;
+      }
+    },
+    startTime(tms) {
+      if (this.createEvent) {
+        this.removeCreateEvent();
+      }
+      const mouse = this.toTime(tms);
+      if (!this.dragEvent) {
+        this.createStart = this.roundTime(mouse);
+        this.createEvent = {
+          name: this.$t("meetings.meeting"),
+          color: "#9dbdf5",
+          start: this.createStart,
+          end: this.createStart,
+          timed: true
+        };
+        this.localEvents.push(this.createEvent);
+      }
+    },
+    mouseMove(tms) {
+      const mouse = this.toTime(tms);
+      if (!this.dragEvent && this.createEvent && this.createStart !== null) {
+        const mouseRounded = this.roundTime(mouse, false);
+        const min = Math.min(mouseRounded, this.createStart);
+        const max = Math.max(mouseRounded, this.createStart);
+
+        this.createEvent.start = min;
+        this.createEvent.end = max;
+      }
+    },
+    endDrag() {
+      if (this.createEvent) {
+        let isValid = true;
+        const start = moment(this.createEvent.start);
+        const end = moment(this.createEvent.end);
+
+        if (start.isSame(end)) {
+          isValid = false;
+        }
+        if (!start.startOf("day").isSame(end.startOf("day"))) {
+          isValid = false;
+        }
+
+        if (isValid) {
+          this.$emit("add-new-meeting", this.createEvent.start, this.createEvent.end);
+        }
+      }
+      this.removeCreateEvent();
+    },
+    cancelDrag() {
+      if (this.createEvent) {
+        const i = this.localEvents.indexOf(this.createEvent);
+        if (i !== -1) {
+          this.localEvents.splice(i, 1);
+        }
+      }
+
+      this.createEvent = null;
+      this.createStart = null;
+      this.dragEvent = null;
+    },
+    removeCreateEvent() {
+      const i = this.localEvents.indexOf(this.createEvent);
+      if (i !== -1) {
+        this.localEvents.splice(i, 1);
+      }
+      this.createEvent = null;
+    },
+    roundTime(time, down = true) {
+      const roundTo = 15; // minutes
+      const roundDownTime = roundTo * 60 * 1000;
+
+      return down
+        ? time - (time % roundDownTime)
+        : time + (roundDownTime - (time % roundDownTime));
+    },
+    toTime(tms) {
+      return new Date(
+        tms.year,
+        tms.month - 1,
+        tms.day,
+        tms.hour,
+        tms.minute
+      ).getTime();
     }
   }
 };
 </script>
 <style lang="scss">
-  .meeting-calendar {
-    &.v-calendar .v-event-timed-container {
-      margin-right: 0;
-    }
-    &.theme--dark.v-calendar-daily,
-    &.theme--light.v-calendar-daily {
-      border-left: 0;
-    }
-    .v-calendar-daily__interval-text {
-      text-align: center;
-    }
-    .v-calendar-daily_head-day-label .v-btn__content {
-      font-size: 2rem;
-    }
+.meeting-calendar {
+  &.v-calendar .v-event-timed-container {
+    margin-right: 0;
   }
+  &.theme--dark.v-calendar-daily,
+  &.theme--light.v-calendar-daily {
+    border-left: 0;
+  }
+  .v-calendar-daily__interval-text {
+    text-align: center;
+  }
+  .v-calendar-daily_head-day-label .v-btn__content {
+    font-size: 2rem;
+  }
+}
 </style>
