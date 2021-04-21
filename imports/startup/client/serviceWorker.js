103e3db189bda7aad59926721a9507b2e47bbe9e
Jérôme Blondon <jerome@blondon.fr>
2021-04-21 11:42:21 +0200

    Do not log service worker registration success

--- imports/startup/client/serviceWorker.js
+++ imports/startup/client/serviceWorker.js
@@ -4,7 +4,6 @@ import { Meteor } from "meteor/meteor";
 Meteor.startup(() => {
   navigator.serviceWorker
     .register("/sw.js")
-    .then(() => console.info("service worker registered"))
     .catch((error) => {
       console.log("ServiceWorker registration failed: ", error);
     });
