5615e40d4400897f6a56166ac3fd328c4ac80e35
Jérôme Blondon <jerome@blondon.fr>
2023-12-03 08:16:48 +0100

    Migrate fixTaskNumber

--- imports/startup/server/fixTaskNumber.js
+++ imports/startup/server/fixTaskNumber.js
@@ -1,17 +1,17 @@
 import { Tasks } from "/imports/api/tasks/tasks.js";
 
-function fixTaskNumber() {
+async function fixTaskNumber() {
   const tasks = Tasks.find({
     number: {
       $exists: false
     }
   });
-  const count = tasks.count();
+  const count = await tasks.countAsync();
   /* eslint no-console: off */
   console.log(`${count} tasks without number`);
-  tasks.forEach((task) => {
-    Meteor.call("tasks.setNumber", task._id);
+  tasks.forEachAsync(async (task) => {
+    await Meteor.callAsync("tasks.setNumber", task._id);
   });
 }
 
-fixTaskNumber();
+await fixTaskNumber();
